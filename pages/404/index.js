0eb29cb3cf764dbc549284e7a5e5af7bff5e3934
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-05 14:31:59 -0400

    Transferred folders and files from next.js demo

---
+++ pages/404/index.js
@@ -0,0 +1,10 @@
+import styles from "./404.module.scss";
+
+export default function Custom404() {
+  return (
+    <div className={styles.frame}>
+      <h1 className={styles.errorTextLarge}>404</h1>
+      <h2 className={styles.errorTextSmall}>Page Not Found</h2>
+    </div>
+  );
+}
