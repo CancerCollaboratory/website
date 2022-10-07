6213699c0ccb53e94f30d6f1a7f26e76a0fe7ead
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-07 15:10:09 -0400

    Added publications and presentations page

---
+++ pages/publications-presentations/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../../../components/navbar/navbar";
+import Footer from "../../../components/footer/footer";
+
+export default function PublicationsAndPresentations() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+}
