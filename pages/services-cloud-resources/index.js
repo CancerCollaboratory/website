0980eb774518d6495da8cb992436c6d284c0f798
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-07 14:41:25 -0400

    Added services pages

---
+++ pages/services-cloud-resources/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
+
+export default function CloudResources() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+}
