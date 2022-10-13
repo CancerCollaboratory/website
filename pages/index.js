eb11285dbcfc57d2e78a72790f45133001f3deee
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-13 12:56:11 -0400

    Used the built-in head component on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Navbar from "../components/navbar/navbar";
 import Footer from "../components/footer/footer";
+import Head from "next/head";
 import { getPageData } from "../lib/pages";
 import { attributesToProps } from "html-react-parser";
 
@@ -20,6 +21,12 @@ export default function Home({ pageData }) {
   return (
     <div>
       <Navbar></Navbar>
+      <Head>
+        <title>
+          Cloud Computing for Big Data Genomics | Cancer Genome Collaboratory
+        </title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Footer></Footer>
     </div>
   );
