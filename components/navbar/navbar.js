da6f85ab3e5e83ff64c2342d61b79571666c5cef
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-13 09:14:27 -0400

    Made the submenu expandable

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -9,12 +9,136 @@ export default function Navbar() {
     if (isExpanded === "false") {
       document.getElementById("navBtn").setAttribute("aria-expanded", "true");
       document.getElementById("navMenu").setAttribute("aria-expanded", "true");
-      isExpanded = "true";
+      setAriaExpandedFalse();
     } else {
       document.getElementById("navBtn").setAttribute("aria-expanded", "false");
       document.getElementById("navMenu").setAttribute("aria-expanded", "false");
-      isExpanded = "false";
     }
+  }
+
+  function setAriaExpandedFalse() {
+    document
+      .getElementById("subMenuAboutItem")
+      .setAttribute("aria-expanded", "false");
+    document
+      .getElementById("subMenuServicesItem")
+      .setAttribute("aria-expanded", "false");
+    document
+      .getElementById("subMenuResearchItem")
+      .setAttribute("aria-expanded", "false");
+    document
+      .getElementById("subMenuSupportItem")
+      .setAttribute("aria-expanded", "false");
+  }
+
+  function collapseSubMenuItem(subMenuItemId) {
+    let isSubMenuAboutExpanded = document
+      .getElementById("subMenuAboutItem")
+      .getAttribute("aria-expanded");
+
+    let isSubMenuServicesExpanded = document
+      .getElementById("subMenuServicesItem")
+      .getAttribute("aria-expanded");
+
+    let isSubMenuResearchExpanded = document
+      .getElementById("subMenuResearchItem")
+      .getAttribute("aria-expanded");
+
+    let isSubMenuSupportExpanded = document
+      .getElementById("subMenuSupportItem")
+      .getAttribute("aria-expanded");
+
+    const subMenuIds = [
+      "subMenuAboutItem",
+      "subMenuServicesItem",
+      "subMenuResearchItem",
+      "subMenuSupportItem",
+    ];
+
+    const filteredSubMenuItemIds = subMenuIds.filter(filterSubMenuItemIds);
+
+    function filterSubMenuItemIds(value) {
+      if (value !== subMenuItemId) {
+        return value;
+      }
+    }
+
+    for (let itemIdIndex = 0; itemIdIndex < 3; itemIdIndex++) {
+      if (filteredSubMenuItemIds[itemIdIndex] === "subMenuAboutItem") {
+        if (isSubMenuAboutExpanded === "true") {
+          document
+            .getElementById("subMenuAboutItem")
+            .setAttribute("aria-expanded", "false");
+          isSubMenuAboutExpanded = "false";
+        }
+      } else if (
+        filteredSubMenuItemIds[itemIdIndex] === "subMenuServicesItem"
+      ) {
+        if (isSubMenuServicesExpanded === "true") {
+          document
+            .getElementById("subMenuServicesItem")
+            .setAttribute("aria-expanded", "false");
+          isSubMenuServicesExpanded = "false";
+        }
+      } else if (
+        filteredSubMenuItemIds[itemIdIndex] === "subMenuResearchItem"
+      ) {
+        if (isSubMenuResearchExpanded === "true") {
+          document
+            .getElementById("subMenuResearchItem")
+            .setAttribute("aria-expanded", "false");
+          isSubMenuResearchExpanded = "false";
+        }
+      } else if (filteredSubMenuItemIds[itemIdIndex] === "subMenuSupportItem") {
+        if (isSubMenuSupportExpanded === "true") {
+          document
+            .getElementById("subMenuSupportItem")
+            .setAttribute("aria-expanded", "false");
+          isSubMenuSupportExpanded = "false";
+        }
+      }
+    }
+  }
+
+  function toggleAriaExpanded(isSubMenuItemExpanded, subMenuItemId) {
+    if (isSubMenuItemExpanded === "false") {
+      document
+        .getElementById(subMenuItemId)
+        .setAttribute("aria-expanded", "true");
+      collapseSubMenuItem(subMenuItemId);
+    } else {
+      document
+        .getElementById(subMenuItemId)
+        .setAttribute("aria-expanded", "false");
+    }
+  }
+
+  function expandAboutSubMenu() {
+    const isSubMenuAboutExpanded = document
+      .getElementById("subMenuAboutItem")
+      .getAttribute("aria-expanded");
+    toggleAriaExpanded(isSubMenuAboutExpanded, "subMenuAboutItem");
+  }
+
+  function expandServicesSubMenu() {
+    const isSubMenuServicesExpanded = document
+      .getElementById("subMenuServicesItem")
+      .getAttribute("aria-expanded");
+    toggleAriaExpanded(isSubMenuServicesExpanded, "subMenuServicesItem");
+  }
+
+  function expandResearchSubMenu() {
+    const isSubMenuResearchExpanded = document
+      .getElementById("subMenuResearchItem")
+      .getAttribute("aria-expanded");
+    toggleAriaExpanded(isSubMenuResearchExpanded, "subMenuResearchItem");
+  }
+
+  function expandSupportSubMenu() {
+    const isSubMenuSupportExpanded = document
+      .getElementById("subMenuSupportItem")
+      .getAttribute("aria-expanded");
+    toggleAriaExpanded(isSubMenuSupportExpanded, "subMenuSupportItem");
   }
 
   return (
@@ -148,9 +272,17 @@ export default function Navbar() {
               placeholder="Search"
             />
           </div>
-          <button type="button" className={styles.expandedMenuItem}>
+          <button
+            type="button"
+            className={styles.expandedMenuItem}
+            onClick={expandAboutSubMenu}
+          >
             <p className={styles.itemName}>About Us ▾</p>
-            <div className={styles.expandedSubMenuItem}>
+            <div
+              className={styles.expandedSubMenuItem}
+              id="subMenuAboutItem"
+              aria-expanded="false"
+            >
               <Link href="/about-collaboratory">About the Collaboratory</Link>
               <Link href="/about-team">Our Team</Link>
               <Link href="/about-funders-collaborators">
@@ -158,21 +290,103 @@ export default function Navbar() {
               </Link>
             </div>
           </button>
-          <button type="button" className={styles.expandedMenuItem}>
+          <button
+            type="button"
+            className={styles.expandedMenuItem}
+            onClick={expandServicesSubMenu}
+          >
             <p className={styles.itemName}>Our Services ▾</p>
-            <div className={styles.expandedSubMenuItem}></div>
+            <div
+              className={styles.expandedSubMenuItem}
+              id="subMenuServicesItem"
+              aria-expanded="false"
+            >
+              <Link href="/services">Our Services</Link>
+              <Link href="/services-cloud-resources">Cloud Resources</Link>
+              <Link href="/services-dockstore">Dockstore</Link>
+              <Link href="/services-request-account">Request an Account</Link>
+            </div>
           </button>
-          <button type="button" className={styles.expandedMenuItem}>
+          <button
+            type="button"
+            className={styles.expandedMenuItem}
+            onClick={expandResearchSubMenu}
+          >
             <p className={styles.itemName}>Our Research ▾</p>
-            <div className={styles.expandedSubMenuItem}></div>
+            <div
+              className={styles.expandedSubMenuItem}
+              id="subMenuResearchItem"
+              aria-expanded="false"
+            >
+              <Link href="/research#technology_development_cores">
+                Technology Development Cores
+              </Link>
+              <Link href="/research/hardware-software-infrastructure">
+                <a className={styles.innerExpandedMenu}>
+                  Hardware & Software Infrastructure
+                </a>
+              </Link>
+              <Link href="/research/benchmarking">
+                <a className={styles.innerExpandedMenu}>Benchmarking</a>
+              </Link>
+              <Link href="/research/training-outreach">
+                <a className={styles.innerExpandedMenu}>Training & Outreach</a>
+              </Link>
+              <Link href="/research/management-finance">
+                <a className={styles.innerExpandedMenu}>Management & Finance</a>
+              </Link>
+              <Link href="/research#research_modules">Research Modules</Link>
+              <Link href="/research/indexing-search-compression">
+                <a className={styles.innerExpandedMenu}>
+                  Indexing, Search & Compression
+                </a>
+              </Link>
+              <Link href="/research/variant-identification-consequence">
+                <a className={styles.innerExpandedMenu}>
+                  Variant Identification & Consequence
+                </a>
+              </Link>
+              <Link href="/research/drug-target-identification">
+                <a className={styles.innerExpandedMenu}>
+                  Drug Target Identification
+                </a>
+              </Link>
+              <Link href="/research/bioethics-protection-phi">
+                <a className={styles.innerExpandedMenu}>
+                  Bioethics & Protection of PHI
+                </a>
+              </Link>
+              <Link href="/publications-presentations">
+                <a className={styles.innerExpandedMenu}>
+                  Publications & Presentations
+                </a>
+              </Link>
+            </div>
           </button>
-          <button type="button" className={styles.expandedMenuItem}>
+          <button
+            type="button"
+            className={styles.expandedMenuItem}
+            onClick={expandSupportSubMenu}
+          >
             <p className={styles.itemName}>Support ▾</p>
-            <div className={styles.expandedSubMenuItem}></div>
+            <div
+              className={styles.expandedSubMenuItem}
+              id="subMenuSupportItem"
+              aria-expanded="false"
+            >
+              <Link href="/support/getting-started">Getting Started</Link>
+              <Link href="/support/best-practices">Best Practices</Link>
+              <Link href="/support/user-guide">User Guide</Link>
+              <Link href="/support/glossary-terms">Glossary of Terms</Link>
+              <Link href="/support/workshops">Workshops</Link>
+              <Link href="/support/faqs">FAQs</Link>
+              <Link href="/support/system-status">System Status</Link>
+            </div>
           </button>
           <button type="button" className={styles.expandedMenuItem}>
-            <p className={styles.itemName}>Contact Us</p>
-            <div className={styles.expandedSubMenuItem}></div>
+            <Link href="/contact-us">
+              <div className={styles.noSubMenu}>Contact Us</div>
+            </Link>
           </button>
         </div>
       </div>
