7875426faf5b244f365963b702f3c18a9b39b258
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-18 10:57:48 -0400

    Implemented React hooks

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,130 +1,16 @@
 import Link from "next/link";
+import { useState, useEffect, useRef } from "react";
 import NavbarLink from "../navbarLink/navbarLink";
 import styles from "./navbar.module.scss";
 
-export default function Navbar() {
-  function expandNavMenu() {
-    let isExpanded = document.getElementById("navBtn").getAttribute("aria-expanded");
-    if (isExpanded === "false") {
-      document.getElementById("navBtn").setAttribute("aria-expanded", "true");
-      document.getElementById("navMenu").setAttribute("aria-expanded", "true");
-      setAriaExpandedFalse();
-    } else {
-      document.getElementById("navBtn").setAttribute("aria-expanded", "false");
-      document.getElementById("navMenu").setAttribute("aria-expanded", "false");
-    }
-  }
-
-  function clickedOutsideExpandedSubMenu() {
-    if (typeof window !== "undefined") {
-      window.addEventListener("click", function (e) {
-        if (!document.getElementById("expandedMenu").contains(e.target)) {
-          setAriaExpandedFalse();
-        }
-      });
-    }
-  }
-
-  function setAriaExpandedFalse() {
-    document.getElementById("subMenuAboutItem").setAttribute("aria-expanded", "false");
-    document.getElementById("subMenuServicesItem").setAttribute("aria-expanded", "false");
-    document.getElementById("subMenuResearchItem").setAttribute("aria-expanded", "false");
-    document.getElementById("subMenuSupportItem").setAttribute("aria-expanded", "false");
-  }
-
-  function collapseSubMenuItem(subMenuItemId) {
-    let isSubMenuAboutExpanded = document
-      .getElementById("subMenuAboutItem")
-      .getAttribute("aria-expanded");
-
-    let isSubMenuServicesExpanded = document
-      .getElementById("subMenuServicesItem")
-      .getAttribute("aria-expanded");
-
-    let isSubMenuResearchExpanded = document
-      .getElementById("subMenuResearchItem")
-      .getAttribute("aria-expanded");
-
-    let isSubMenuSupportExpanded = document
-      .getElementById("subMenuSupportItem")
-      .getAttribute("aria-expanded");
-
-    const subMenuIds = [
-      "subMenuAboutItem",
-      "subMenuServicesItem",
-      "subMenuResearchItem",
-      "subMenuSupportItem",
-    ];
-
-    const filteredSubMenuItemIds = subMenuIds.filter(filterSubMenuItemIds);
-
-    function filterSubMenuItemIds(value) {
-      if (value !== subMenuItemId) {
-        return value;
-      }
-    }
-
-    for (let itemIdIndex = 0; itemIdIndex < 3; itemIdIndex++) {
-      if (filteredSubMenuItemIds[itemIdIndex] === "subMenuAboutItem") {
-        if (isSubMenuAboutExpanded === "true") {
-          document.getElementById("subMenuAboutItem").setAttribute("aria-expanded", "false");
-          isSubMenuAboutExpanded = "false";
-        }
-      } else if (filteredSubMenuItemIds[itemIdIndex] === "subMenuServicesItem") {
-        if (isSubMenuServicesExpanded === "true") {
-          document.getElementById("subMenuServicesItem").setAttribute("aria-expanded", "false");
-          isSubMenuServicesExpanded = "false";
-        }
-      } else if (filteredSubMenuItemIds[itemIdIndex] === "subMenuResearchItem") {
-        if (isSubMenuResearchExpanded === "true") {
-          document.getElementById("subMenuResearchItem").setAttribute("aria-expanded", "false");
-          isSubMenuResearchExpanded = "false";
-        }
-      } else if (filteredSubMenuItemIds[itemIdIndex] === "subMenuSupportItem") {
-        if (isSubMenuSupportExpanded === "true") {
-          document.getElementById("subMenuSupportItem").setAttribute("aria-expanded", "false");
-          isSubMenuSupportExpanded = "false";
-        }
-      }
-    }
-  }
-
-  function toggleAriaExpanded(isSubMenuItemExpanded, subMenuItemId) {
-    if (isSubMenuItemExpanded === "false") {
-      document.getElementById(subMenuItemId).setAttribute("aria-expanded", "true");
-      collapseSubMenuItem(subMenuItemId);
-    } else {
-      document.getElementById(subMenuItemId).setAttribute("aria-expanded", "false");
-    }
-  }
-
-  function expandAboutSubMenu() {
-    const isSubMenuAboutExpanded = document
-      .getElementById("subMenuAboutItem")
-      .getAttribute("aria-expanded");
-    toggleAriaExpanded(isSubMenuAboutExpanded, "subMenuAboutItem");
-  }
-
-  function expandServicesSubMenu() {
-    const isSubMenuServicesExpanded = document
-      .getElementById("subMenuServicesItem")
-      .getAttribute("aria-expanded");
-    toggleAriaExpanded(isSubMenuServicesExpanded, "subMenuServicesItem");
-  }
-
-  function expandResearchSubMenu() {
-    const isSubMenuResearchExpanded = document
-      .getElementById("subMenuResearchItem")
-      .getAttribute("aria-expanded");
-    toggleAriaExpanded(isSubMenuResearchExpanded, "subMenuResearchItem");
-  }
-
-  function expandSupportSubMenu() {
-    const isSubMenuSupportExpanded = document
-      .getElementById("subMenuSupportItem")
-      .getAttribute("aria-expanded");
-    toggleAriaExpanded(isSubMenuSupportExpanded, "subMenuSupportItem");
-  }
+const Navbar = () => {
+  const [isToggled, setIsToggled] = useState(false);
+  const [isSubMenuAboutExpanded, setIsSubMenuAboutExpanded] = useState(false);
+  const [isSubMenuServicesExpanded, setIsSubMenuServicesExpanded] = useState(false);
+  const [isSubMenuResearchExpanded, setIsSubMenuResearchExpanded] = useState(false);
+  const [isSubMenuSupportExpanded, setIsSubMenuSupportExpanded] = useState(false);
+  const [expandedMenu, setExpandedMenu] = useState(null);
+  const menuDropdown = useRef(null);
 
   const navbarLinksAbout = [
     {
@@ -233,7 +119,59 @@ export default function Navbar() {
     },
   ];
 
-  clickedOutsideExpandedSubMenu();
+  function expandNavMenu() {
+    if (!isToggled) {
+      setIsToggled(true);
+      setAriaExpandedFalse();
+    } else {
+      setIsToggled(false);
+    }
+  }
+
+  function setAriaExpandedFalse() {
+    setIsSubMenuAboutExpanded(false);
+    setIsSubMenuServicesExpanded(false);
+    setIsSubMenuResearchExpanded(false);
+    setIsSubMenuSupportExpanded(false);
+  }
+
+  function expandAboutSubMenu() {
+    !isSubMenuAboutExpanded ? setIsSubMenuAboutExpanded(true) : setIsSubMenuAboutExpanded(false);
+  }
+
+  function expandServicesSubMenu() {
+    !isSubMenuServicesExpanded
+      ? setIsSubMenuServicesExpanded(true)
+      : setIsSubMenuServicesExpanded(false);
+  }
+
+  function expandResearchSubMenu() {
+    !isSubMenuResearchExpanded
+      ? setIsSubMenuResearchExpanded(true)
+      : setIsSubMenuResearchExpanded(false);
+  }
+
+  function expandSupportSubMenu() {
+    !isSubMenuSupportExpanded
+      ? setIsSubMenuSupportExpanded(true)
+      : setIsSubMenuSupportExpanded(false);
+  }
+
+  function collapseSubMenu() {
+    window.addEventListener("click", function (e) {
+      if (!expandedMenu.contains(e.target)) {
+        setAriaExpandedFalse();
+      }
+    });
+  }
+
+  useEffect(() => {
+    setExpandedMenu(menuDropdown.current);
+  }, []);
+
+  useEffect(() => {
+    expandedMenu && collapseSubMenu();
+  });
 
   return (
     <div className={styles.navbarContainer}>
@@ -285,8 +223,7 @@ export default function Navbar() {
           type="button"
           className={styles.navbarToggler}
           onClick={expandNavMenu}
-          id="navBtn"
-          aria-expanded="false"
+          aria-expanded={isToggled.toString()}
         >
           <span className={styles.iconBar}></span>
           <span className={styles.iconBar}></span>
@@ -358,7 +295,7 @@ export default function Navbar() {
           </div>
         </div>
       </div>
-      <div className={styles.navDropdown} id="navMenu" aria-expanded="false">
+      <div className={styles.navDropdown} aria-expanded={isToggled.toString()}>
         <div className={styles.expandedMenuContainer}>
           <div className={styles.extendSearchBar}>
             <input
@@ -368,13 +305,12 @@ export default function Navbar() {
               placeholder="Search"
             />
           </div>
-          <div id="expandedMenu" className={styles.wrapExpandedMenu}>
+          <div className={styles.wrapExpandedMenu} ref={menuDropdown}>
             <button type="button" className={styles.expandedMenuItem} onClick={expandAboutSubMenu}>
               <p className={styles.itemName}>About Us ▾</p>
               <div
                 className={styles.expandedSubMenuItem}
-                id="subMenuAboutItem"
-                aria-expanded="false"
+                aria-expanded={isSubMenuAboutExpanded.toString()}
               >
                 {navbarLinksAbout.map((navLink, index) => (
                   <NavbarLink key={index} link={navLink.link} label={navLink.label} />
@@ -389,8 +325,7 @@ export default function Navbar() {
               <p className={styles.itemName}>Our Services ▾</p>
               <div
                 className={styles.expandedSubMenuItem}
-                id="subMenuServicesItem"
-                aria-expanded="false"
+                aria-expanded={isSubMenuServicesExpanded.toString()}
               >
                 {navbarLinksServices.map((navLink, index) => (
                   <NavbarLink key={index} link={navLink.link} label={navLink.label} />
@@ -405,8 +340,7 @@ export default function Navbar() {
               <p className={styles.itemName}>Our Research ▾</p>
               <div
                 className={styles.expandedSubMenuItem}
-                id="subMenuResearchItem"
-                aria-expanded="false"
+                aria-expanded={isSubMenuResearchExpanded.toString()}
               >
                 <NavbarLink
                   link={"/research#technology_development_cores"}
@@ -437,8 +371,7 @@ export default function Navbar() {
               <p className={styles.itemName}>Support ▾</p>
               <div
                 className={styles.expandedSubMenuItem}
-                id="subMenuSupportItem"
-                aria-expanded="false"
+                aria-expanded={isSubMenuSupportExpanded.toString()}
               >
                 {navbarLinksSupport.map((navLink, index) => (
                   <NavbarLink key={index} link={navLink.link} label={navLink.label} />
@@ -455,4 +388,6 @@ export default function Navbar() {
       </div>
     </div>
   );
-}
+};
+
+export default Navbar;
