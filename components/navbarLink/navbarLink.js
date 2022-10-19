e4a73c32631c3a656722190938acbba2f002177b
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-19 18:01:46 -0400

    Built a navbar-item-link component

--- components/navbarLink/navbarLink.js
+++ components/navbarLink/navbarLink.js
@@ -1,7 +1,54 @@
 import Link from "next/link";
+import Image from "next/future/image";
 
-const NavbarLink = ({ link, label }) => {
-  return <Link href={link}>{label}</Link>;
+const NavbarLink = ({
+  label,
+  isExternalLink,
+  link,
+  isImage,
+  srcPath,
+  hasPriority,
+  styleImage,
+  width,
+  height,
+}) => {
+  return (
+    <>
+      {isImage ? (
+        isExternalLink ? (
+          <a href={link} target="_blank">
+            <Image
+              src={srcPath}
+              alt={label}
+              priority={hasPriority}
+              className={styleImage}
+              width={width}
+              height={height}
+            />
+          </a>
+        ) : (
+          <Link href={link}>
+            <>
+              <Image
+                src={srcPath}
+                alt={label}
+                priority={hasPriority}
+                className={styleImage}
+                width={width}
+                height={height}
+              />
+            </>
+          </Link>
+        )
+      ) : isExternalLink ? (
+        <a href={link} target="_blank">
+          {label}
+        </a>
+      ) : (
+        <Link href={link}>{label}</Link>
+      )}
+    </>
+  );
 };
 
 export default NavbarLink;
