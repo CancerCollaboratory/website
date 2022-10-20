69276eca320b6526811b38b1f07cbe5992dc6e0d
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-19 21:01:29 -0400

    Fixed a bug

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,10 @@ import "../styles/reset.scss";
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || (page => page);
 
-  if (getLayout(<Component {...pageProps} />).type.length > 0) {
+  if (
+    getLayout(<Component {...pageProps} />).type.length > 0 &&
+    getLayout(<Component {...pageProps} />).type.name !== "Home"
+  ) {
     return getLayout(<Component {...pageProps} />);
   } else {
     return (
