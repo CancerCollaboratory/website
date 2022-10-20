d089b2a052304c1166d0f7a1c14cc4b26f98521a
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-19 20:35:15 -0400

    Wrapped the application with layouts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Layout from "../components/layout/layout";
 import "../styles/reset.scss";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const getLayout = Component.getLayout || (page => page);
+
+  if (getLayout(<Component {...pageProps} />).type.length > 0) {
+    return getLayout(<Component {...pageProps} />);
+  } else {
+    return (
+      <Layout>
+        (<Component {...pageProps} />)
+      </Layout>
+    );
+  }
 }
