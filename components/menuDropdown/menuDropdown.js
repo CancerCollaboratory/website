e603457385a019d440563e4f0e2c7217fbbfad4a
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-20 10:05:35 -0400

    Implemented conditional rendering

--- components/menuDropdown/menuDropdown.js
+++ components/menuDropdown/menuDropdown.js
@@ -1,26 +1,30 @@
 import NavbarLink from "../navbarLink/navbarLink";
 import styles from "../navbar/navbar.module.scss";
 
-const MenuDropdown = ({ label, hasLink, link, styleSubMenu, items }) => {
+const MenuDropdown = ({ label, hasLink, link, styleSubMenu, items, isHovered }) => {
   return (
     <>
       {hasLink ? (
         <>
           <NavbarLink label={label} link={link} />
-          <div className={styleSubMenu}>
-            {items?.map((item, index) => (
-              <NavbarLink key={index} label={item.label} link={item.link} />
-            ))}
-          </div>
+          {isHovered && (
+            <div className={styleSubMenu}>
+              {items?.map((item, index) => (
+                <NavbarLink key={index} label={item.label} link={item.link} />
+              ))}
+            </div>
+          )}
         </>
       ) : (
         <>
           <p className={styles.itemName}>{label}</p>
-          <div className={styleSubMenu}>
-            {items?.map((item, index) => (
-              <NavbarLink key={index} label={item.label} link={item.link} />
-            ))}
-          </div>
+          {isHovered && (
+            <div className={styleSubMenu}>
+              {items?.map((item, index) => (
+                <NavbarLink key={index} label={item.label} link={item.link} />
+              ))}
+            </div>
+          )}
         </>
       )}
     </>
