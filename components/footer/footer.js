572f1a31026f735366773d75caebeeb7ce8539b9
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-20 13:00:52 -0400

    Edited the existing footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -2,8 +2,9 @@ import styles from "./footer.module.scss";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <p>This is a temporary footer.</p>
+    <div className={styles.footerContainer}>
+      <div className={styles.footerHeadContainer}></div>
+      <div className={styles.footerTailContainer}></div>
     </div>
   );
 };
