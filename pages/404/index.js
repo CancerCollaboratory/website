21b126c2675a9e5ccc8099e58381224daf67cef5
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-19 21:35:41 -0400

    Added support for a custom layout for the 404 page

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,12 +1,17 @@
+import Layout404 from "../../components/layout404/layout404";
 import styles from "./404.module.scss";
 
 const Custom404 = () => {
   return (
-    <div className={styles.frame}>
+    <>
       <h1 className={styles.errorTextLarge}>404</h1>
-      <h2 className={styles.errorTextSmall}>Page Not Found</h2>
-    </div>
+      <h2 className={styles.errorTextMedium}>Page Not Found</h2>
+    </>
   );
 };
 
 export default Custom404;
+
+Custom404.getLayout = function getLayout(page) {
+  return <Layout404>{page}</Layout404>;
+};
