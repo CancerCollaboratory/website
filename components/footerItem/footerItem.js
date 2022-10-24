32fa29c887b876d94994d67d970276f3d800b86a
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-24 15:08:36 -0400

    Used the FooterItem component

---
+++ components/footerItem/footerItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+import clsx from "clsx";
+import NavbarLink from "../navbarLink/navbarLink";
+import styles from "../footer/footer.module.scss";
+
+const FooterItem = ({ label, labelLink, footerItem }) => {
+  return (
+    <div
+      className={clsx(
+        styles.footerItemContainer,
+        footerItem.subItems && styles.footerItemRightBorder
+      )}
+    >
+      <NavbarLink label={label} link={labelLink} />
+      {footerItem.subItems?.map((subItem, index) => (
+        <ul key={index}>
+          <li>
+            <NavbarLink label={subItem.label} link={subItem.link} />
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default FooterItem;
