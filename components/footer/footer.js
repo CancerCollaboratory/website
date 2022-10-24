9b01d2c29756a5f56311db4527b9a57fac6eb6b0
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-24 15:11:35 -0400

    Used footer data and the FooterItem component

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,10 +1,16 @@
 import styles from "./footer.module.scss";
+import FooterItem from "../footerItem/footerItem";
+import data from "../../content/components/footerItems.json";
 
 const Footer = () => {
   return (
     <div className={styles.footerContainer}>
       <div className={styles.footerHeadContainer}></div>
-      <div className={styles.footerTailContainer}></div>
+      <div className={styles.footerTailContainer}>
+        {data.footerItems.map((item, index) => (
+          <FooterItem label={item.label} labelLink={item.link} footerItem={item} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
