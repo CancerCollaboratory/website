aa919f1da670744bdd73ea0a45606e07c79e4c97
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-25 11:00:07 -0400

    Expanded the FooterItem component

--- components/footerItem/footerItem.js
+++ components/footerItem/footerItem.js
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import NavbarLink from "../navbarLink/navbarLink";
 import styles from "../footer/footer.module.scss";
+import utilStyles from "../../styles/utils.module.scss";
 
 const FooterItem = ({ label, labelLink, footerItem }) => {
   return (
@@ -10,14 +11,29 @@ const FooterItem = ({ label, labelLink, footerItem }) => {
         footerItem.subItems && styles.footerItemRightBorder
       )}
     >
-      <NavbarLink label={label} link={labelLink} />
-      {footerItem.subItems?.map((subItem, index) => (
-        <ul key={index}>
-          <li>
-            <NavbarLink label={subItem.label} link={subItem.link} />
-          </li>
-        </ul>
-      ))}
+      <NavbarLink label={label} link={labelLink} styleLink={utilStyles.itemLabel} />
+      <ul
+        className={clsx(
+          styles.subItemsList,
+          footerItem.subItems?.length === 2 && styles.subItemsRow
+        )}
+      >
+        {footerItem.subItems?.map((subItem, subItemIndex) =>
+          !subItem?.column ? (
+            <li key={subItemIndex}>
+              <NavbarLink label={subItem.label} link={subItem.link} />
+            </li>
+          ) : (
+            <div className={subItemIndex === 0 && styles.columnPadding}>
+              {subItem.column.map((columnItem, columnIndex) => (
+                <li key={columnIndex}>
+                  <NavbarLink label={columnItem.label} link={columnItem.link} />
+                </li>
+              ))}
+            </div>
+          )
+        )}
+      </ul>
     </div>
   );
 };
