004471f0fc70d6ce9bf281859422d3da80fe3bf3
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-26 13:09:54 -0400

    Expanded the footer to include the head

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,13 +1,18 @@
 import styles from "./footer.module.scss";
+import FooterHead from "../footerHead/footerHead";
 import FooterItem from "../footerItem/footerItem";
 import data from "../../content/components/footerItems.json";
 
 const Footer = () => {
   return (
     <div className={styles.footerContainer}>
-      <div className={styles.footerHeadContainer}></div>
+      <div className={styles.footerHeadContainer}>
+        {data.footerHeadItems.map((item, index) => (
+          <FooterHead headItem={item} key={index} />
+        ))}
+      </div>
       <div className={styles.footerTailContainer}>
-        {data.footerItems.map((item, index) => (
+        {data.footerTailItems.map((item, index) => (
           <FooterItem label={item.label} labelLink={item.link} footerItem={item} key={index} />
         ))}
       </div>
