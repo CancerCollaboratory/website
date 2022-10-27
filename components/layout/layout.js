f6c2448efcd2ff22629ec1541921307c8bcc300f
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-27 17:09:47 -0400

    Changed the layout for pages

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,12 @@
 import Navbar from "../navbar/navbar";
+import SubNavbar from "../subNavbar/subNavbar";
 import Footer from "../footer/footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <SubNavbar />
       {children}
       <Footer />
     </>
