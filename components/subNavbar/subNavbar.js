229813dca1fc8fc1e8b871922b4362f3c2a4fcc9
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-27 23:43:45 -0400

    Fixed a conditional bug

--- components/subNavbar/subNavbar.js
+++ components/subNavbar/subNavbar.js
@@ -25,38 +25,48 @@ const SubNavbar = () => {
 
   return (
     <>
-      {asPath !== data.navbarItems[3].link && asPath !== data.navbarItems[4].link && (
-        <ul className={styles.subNavbarContainer}>
-          {data.navbarItems[activeNavbarItemIndex].navbarSubMenuItems?.map((item, index) => (
-            <li key={index}>
-              <NavbarLink
-                label={item.label}
-                link={item.link}
-                styleLink={
-                  asPath === item.link ||
-                  ((asPath === data.navbarItems[2].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[0].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[1].link) &&
-                    item.link !== data.navbarItems[2].navbarSubMenuItems[2].link) ||
-                  ((asPath === data.navbarItems[2].navbarSubMenuItems[0].subMenuSection[0].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[0].subMenuSection[1].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[0].subMenuSection[2].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[0].subMenuSection[3].link) &&
-                    item.link === data.navbarItems[2].navbarSubMenuItems[0].link) ||
-                  ((asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[0].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[1].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[2].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[3].link ||
-                    asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[4].link) &&
-                    item.link === data.navbarItems[2].navbarSubMenuItems[1].link)
-                    ? styles.selectedSubNavbarItem
-                    : undefined
-                }
-              />
-            </li>
-          ))}
-        </ul>
-      )}
+      {asPath !== data.navbarItems[3].link &&
+        asPath !== data.navbarItems[3].navbarSubMenuItems[0].link &&
+        asPath !== data.navbarItems[3].navbarSubMenuItems[1].link &&
+        asPath !== data.navbarItems[3].navbarSubMenuItems[2].link &&
+        asPath !== data.navbarItems[3].navbarSubMenuItems[3].link &&
+        asPath !== data.navbarItems[3].navbarSubMenuItems[4].link &&
+        asPath !== data.navbarItems[3].navbarSubMenuItems[5].link &&
+        asPath !== data.navbarItems[3].navbarSubMenuItems[6].link &&
+        asPath !== data.navbarItems[4].link && (
+          <ul className={styles.subNavbarContainer}>
+            {data.navbarItems[activeNavbarItemIndex].navbarSubMenuItems?.map((item, index) => (
+              <li key={index}>
+                <NavbarLink
+                  label={item.label}
+                  link={item.link}
+                  styleLink={
+                    asPath === item.link ||
+                    ((asPath === data.navbarItems[2].link ||
+                      asPath === data.navbarItems[2].navbarSubMenuItems[0].link ||
+                      asPath === data.navbarItems[2].navbarSubMenuItems[1].link) &&
+                      item.link !== data.navbarItems[2].navbarSubMenuItems[2].link) ||
+                    ((asPath === data.navbarItems[2].navbarSubMenuItems[0].subMenuSection[0].link ||
+                      asPath === data.navbarItems[2].navbarSubMenuItems[0].subMenuSection[1].link ||
+                      asPath === data.navbarItems[2].navbarSubMenuItems[0].subMenuSection[2].link ||
+                      asPath ===
+                        data.navbarItems[2].navbarSubMenuItems[0].subMenuSection[3].link) &&
+                      item.link === data.navbarItems[2].navbarSubMenuItems[0].link) ||
+                    ((asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[0].link ||
+                      asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[1].link ||
+                      asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[2].link ||
+                      asPath === data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[3].link ||
+                      asPath ===
+                        data.navbarItems[2].navbarSubMenuItems[1].subMenuSection[4].link) &&
+                      item.link === data.navbarItems[2].navbarSubMenuItems[1].link)
+                      ? styles.selectedSubNavbarItem
+                      : undefined
+                  }
+                />
+              </li>
+            ))}
+          </ul>
+        )}
     </>
   );
 };
