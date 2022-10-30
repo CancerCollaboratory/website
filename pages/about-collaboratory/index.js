d2a9455fa7f5d4bcaef017fed2624b04dc88e68f
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-30 06:01:08 -0400

    Used the Jumbotron component

--- pages/about-collaboratory/index.js
+++ pages/about-collaboratory/index.js
@@ -1,5 +1,21 @@
-const AboutCollaboratory = () => {
-  return <></>;
+import { getPageData } from "../../lib/pages";
+import Jumbotron from "../../components/jumbotron/jumbotron";
+
+export async function getStaticProps() {
+  const pageData = getPageData("about", "about-collaboratory");
+  return {
+    props: {
+      pageData,
+    },
+  };
+}
+
+const AboutCollaboratory = ({ pageData }) => {
+  return (
+    <>
+      <Jumbotron data={pageData} />
+    </>
+  );
 };
 
 export default AboutCollaboratory;
