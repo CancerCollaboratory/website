dda14ccfd9274cef9f3fb7257a7a821bb71ebe74
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-31 07:50:48 -0400

    Removed the setup to parse and get JSON data from a function

--- pages/about-collaboratory/index.js
+++ pages/about-collaboratory/index.js
@@ -2,7 +2,6 @@ import Jumbotron from "../../components/jumbotron/jumbotron";
 import pageData from "../../data/pages/sections/about/about-collaboratory.json";
 
 const AboutCollaboratory = () => {
-  console.log(pageData);
   return (
     <>
       <Jumbotron data={pageData} />
