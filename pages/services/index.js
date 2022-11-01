eba4b66ed0cc420103acfda660ba9d8e7bd8473d
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-01 17:35:58 -0400

    Made a minor change

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,12 +1,10 @@
 import pageData from "../../data/pages/services/services.json";
 import Jumbotron from "../../components/jumbotron/jumbotron";
-import LateralColumn from "../../components/lateralColumn/lateralColumn";
 
 const Services = () => {
   return (
     <>
       <Jumbotron data={pageData} />
-      <LateralColumn data={pageData} />
     </>
   );
 };
