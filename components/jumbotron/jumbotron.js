8ae2cbf1eac573b7376a7923dd3c0694b96b2d60
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-10-31 20:19:44 -0400

    Added support for the use of images in the Jumbotron component

--- components/jumbotron/jumbotron.js
+++ components/jumbotron/jumbotron.js
@@ -1,7 +1,9 @@
 import clsx from "clsx";
 import parse from "html-react-parser";
+import Image from "next/future/image";
+import NavbarLink from "../navbarLink/navbarLink";
 import { micromark } from "micromark";
-import { insertExternalLink, insertInternalLink } from "../../lib/functions";
+import { insertLinkExternal, insertLinkInternal } from "../../lib/functions";
 import styles from "./jumbotron.module.scss";
 
 const Jumbotron = ({ data }) => {
@@ -14,12 +16,34 @@ const Jumbotron = ({ data }) => {
     >
       <h1 className={styles.jumbotronTitle}>{data.jumbotron.title}</h1>
       {data.jumbotron.body && (
-        <div className={styles.jumbotronBody}>
-          {parse(
-            micromark(data.jumbotron.body),
-            data.jumbotron.isLinkExternal === "true" ? insertExternalLink : insertInternalLink
+        <>
+          <div className={styles.jumbotronBody}>
+            {parse(
+              micromark(data.jumbotron.body),
+              data.jumbotron.isLinkExternal === "true" ? insertLinkExternal : insertLinkInternal
+            )}
+          </div>
+          {data.jumbotron.imageSrcPath && (
+            <div className={styles.jumbotronBottom}>
+              <Image
+                src={data.jumbotron.imageSrcPath}
+                alt={data.jumbotron.imageLabel}
+                className={styles.jumbotronImage}
+                priority={false}
+                width={75}
+                height={52}
+                quality={100}
+              />
+              <NavbarLink
+                label={data.jumbotron.buttonLabel}
+                isLinkWrapped={true}
+                styleWrap={styles.jumbotronButton}
+                isLinkExternal={data.jumbotron.isLinkExternal === "true"}
+                link={data.jumbotron.buttonLink}
+              />
+            </div>
           )}
-        </div>
+        </>
       )}
     </div>
   );
