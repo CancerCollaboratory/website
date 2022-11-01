63c317dfad19e571c9825b8bbe36d43016a77002
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-01 00:38:12 -0400

    Improved styling logic

--- components/subNavbar/subNavbar.js
+++ components/subNavbar/subNavbar.js
@@ -1,17 +1,16 @@
 import { useRouter } from "next/router";
 import NavbarLink from "../navbarLink/navbarLink";
-import data from "../../content/components/navbarItems.json";
+import navData from "../../data/components/navbarItems.json";
+import footerData from "../../data/components/footerItems.json";
 import styles from "./subNavbar.module.scss";
 import React, { useEffect, useState } from "react";
 
 const SubNavbar = () => {
   const { asPath } = useRouter();
   const [activeNavbarItemIndex, setActiveNavbarItemIndex] = useState(0);
-  const researchSectionA = data.navbarItems[2].navbarSubMenuItems[0];
-  const researchSectionB = data.navbarItems[2].navbarSubMenuItems[1];
 
   function getNavbarItemIndex(route) {
-    data.navbarItems.map((item, index) =>
+    navData.navbarItems.map((item, index) =>
       item.navbarSubMenuItems?.map(
         subItem =>
           (subItem.link === route || item.link === route) && setActiveNavbarItemIndex(index)
@@ -20,43 +19,34 @@ const SubNavbar = () => {
   }
 
   useEffect(() => {
-    asPath.slice(0, data.navbarItems[3].reducedSlug.length) !== data.navbarItems[3].reducedSlug &&
-      asPath !== data.navbarItems[4].link &&
+    asPath.slice(0, navData.navbarItems[3].reducedSlug.length) !==
+      navData.navbarItems[3].reducedSlug &&
+      asPath !== navData.navbarItems[4].link &&
       asPath !== "/" &&
-      asPath !== "/privacy-policy";
+      asPath !== footerData.footerHeadItems[1].links[1].link;
     getNavbarItemIndex(asPath);
   });
 
   return (
     <>
-      {asPath.slice(0, data.navbarItems[3].reducedSlug.length) !==
-        data.navbarItems[3].reducedSlug &&
-        asPath !== data.navbarItems[4].link &&
+      {asPath.slice(0, navData.navbarItems[3].reducedSlug.length) !==
+        navData.navbarItems[3].reducedSlug &&
+        asPath !== navData.navbarItems[4].link &&
         asPath !== "/" &&
-        asPath !== "/privacy-policy" && (
+        asPath !== footerData.footerHeadItems[1].links[1].link && (
           <ul className={styles.subNavbarContainer}>
-            {data.navbarItems[activeNavbarItemIndex].navbarSubMenuItems?.map((item, index) => (
+            {navData.navbarItems[activeNavbarItemIndex].navbarSubMenuItems?.map((item, index) => (
               <li key={index}>
                 <NavbarLink
                   label={item.label}
                   link={item.link}
                   styleLink={
-                    asPath === item.link ||
-                    (item.link.length > asPath.length &&
-                      asPath + "#" === item.link.slice(0, asPath.length + 1)) ||
-                    asPath.slice(0, data.navbarItems[2].link.length + 1) ===
-                      item.link.slice(0, data.navbarItems[2].link.length) + "#" ||
-                    ((asPath === researchSectionA.subMenuSection[0].link ||
-                      asPath === researchSectionA.subMenuSection[1].link ||
-                      asPath === researchSectionA.subMenuSection[2].link ||
-                      asPath === researchSectionA.subMenuSection[3].link) &&
-                      item.link === researchSectionA.link) ||
-                    ((asPath === researchSectionB.subMenuSection[0].link ||
-                      asPath === researchSectionB.subMenuSection[1].link ||
-                      asPath === researchSectionB.subMenuSection[2].link ||
-                      asPath === researchSectionB.subMenuSection[3].link ||
-                      asPath === researchSectionB.subMenuSection[4].link) &&
-                      item.link === researchSectionB.link)
+                    item.link === asPath ||
+                    asPath + "#" === item.link.slice(0, asPath.length + 1) ||
+                    asPath.slice(0, navData.navbarItems[activeNavbarItemIndex].link.length + 1) ===
+                      item.link.slice(0, navData.navbarItems[activeNavbarItemIndex].link.length) +
+                        "#" ||
+                    item.subMenuSection?.find(element => element.link === asPath)
                       ? styles.selectedSubNavbarItem
                       : undefined
                   }
