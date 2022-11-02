1b9a3d7d1730217208386bfc32529dec25ffa149
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-02 17:05:55 -0400

    Used Table component

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,13 +1,16 @@
 import pageData from "../../data/pages/services/services.json";
 import Jumbotron from "../../components/jumbotron/jumbotron";
 import LateralColumn from "../../components/lateralColumn/lateralColumn";
+import Table from "../../components/table/table";
 
 const Services = () => {
   return (
     <>
       <Jumbotron data={pageData} />
       <LateralColumn data={pageData.lateralColumn1} />
-      <LateralColumn data={pageData.lateralColumn2} />
+      <LateralColumn data={pageData.lateralColumn2}>
+        <Table />
+      </LateralColumn>
     </>
   );
 };
