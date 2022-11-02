1a6281537aff057f755b9f9f3c38928f0962a655
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-02 14:48:33 -0400

    Used LateralColumn

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,10 +1,13 @@
 import pageData from "../../data/pages/services/services.json";
 import Jumbotron from "../../components/jumbotron/jumbotron";
+import LateralColumn from "../../components/lateralColumn/lateralColumn";
 
 const Services = () => {
   return (
     <>
       <Jumbotron data={pageData} />
+      <LateralColumn data={pageData.lateralColumn1} />
+      <LateralColumn data={pageData.lateralColumn2} />
     </>
   );
 };
