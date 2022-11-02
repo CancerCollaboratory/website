8b76f5d7601f924860c0c7188330547e5f31b39f
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-02 10:44:29 -0400

    Isolated logic

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -70,11 +70,7 @@ const Navbar = () => {
           <div className={styles.navbarItemsLine}>
             {data.navbarItems.map((item, index) => (
               <NavbarItem
-                itemLabel={
-                  item.label !== data.navbarItems[4].label
-                    ? item.label + data.itemCaret
-                    : item.label
-                }
+                itemLabel={item.label}
                 isItemlLink={true}
                 itemLink={item.link}
                 styleItemMenu={styles.subMenuDropdown}
@@ -96,7 +92,7 @@ const Navbar = () => {
               {data.navbarItems.map((item, index) =>
                 item.navbarSubMenuItems ? (
                   <NavbarItem
-                    itemLabel={item.label + data.itemCaret}
+                    itemLabel={item.label}
                     isItemlLink={false}
                     styleItemMenu={styles.expandedSubMenuItem}
                     styleItemSubMenu={styles.innerExpandedMenu}
