350d706663438fb8a17b355d540447cd2e0acc76
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-03 12:02:34 -0400

    Passed a style to LateralColumn

--- pages/services/index.js
+++ pages/services/index.js
@@ -2,13 +2,17 @@ import pageData from "../../data/pages/services/services.json";
 import Jumbotron from "../../components/jumbotron/jumbotron";
 import LateralColumn from "../../components/lateralColumn/lateralColumn";
 import Table from "../../components/table/table";
+import utilStyles from "../../styles/utils.module.scss";
 
 const Services = () => {
   return (
     <>
       <Jumbotron data={pageData} />
       <LateralColumn data={pageData.lateralColumn1} />
-      <LateralColumn data={pageData.lateralColumn2}>
+      <LateralColumn
+        data={pageData.lateralColumn2}
+        styleBackground={utilStyles.sectionContainerBackground}
+      >
         <Table data={pageData.table1} />
       </LateralColumn>
     </>
