7d6296514a48a8e0c3af135437b266f91ff94a7e
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-03 17:55:19 -0400

    Finalized Table structure

--- components/table/table.js
+++ components/table/table.js
@@ -5,30 +5,33 @@ import styles from "./table.module.scss";
 
 const Table = ({ data }) => {
   return (
-    <table className={styles.table}>
-      <tbody>
-        {data.map((item, index) => (
-          <tr className={styles.tableRow} key={index}>
-            {item.row.map((subItem, subIndex) => (
-              <td className={styles.tableCell} key={subIndex}>
-                {subItem.cellHead &&
-                  parse(
-                    micromark(subItem.cellHead.title),
-                    subItem.cellHead.isLinkExternal === "true"
-                      ? insertLinkExternal
-                      : insertLinkInternal
+    <>
+      {data.tableHead?.title && <p className={styles.tableTitle}>{data.tableHead.title}</p>}
+      <table className={styles.table}>
+        <tbody>
+          {data.tableBody.map((item, index) => (
+            <tr key={index}>
+              {item.row.map((subItem, subIndex) => (
+                <td className={styles.tableCell} key={subIndex}>
+                  {subItem.cellHead &&
+                    parse(
+                      micromark(subItem.cellHead.title),
+                      subItem.cellHead.isLinkExternal === "true"
+                        ? insertLinkExternal
+                        : insertLinkInternal
+                    )}
+                  {subItem.cellBody && (
+                    <div className={styles.cellBodyContent}>
+                      {parse(micromark(subItem.cellBody.content))}
+                    </div>
                   )}
-                {subItem.cellBody && (
-                  <div className={styles.cellBodyContent}>
-                    {parse(micromark(subItem.cellBody.content))}
-                  </div>
-                )}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
