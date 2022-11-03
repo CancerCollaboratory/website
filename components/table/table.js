eb141127ab95e91c0b7f669db45b05bb29dbb814
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-03 14:10:00 -0400

    Restructured Table component

--- components/table/table.js
+++ components/table/table.js
@@ -1,46 +1,46 @@
 import clsx from "clsx";
-import { getNameOfMonth, getCurrentYear } from "../../lib/functions/timeValues";
 import styles from "./table.module.scss";
+import NavbarLink from "../navbarLink/navbarLink";
 
 const Table = ({ data }) => {
   return (
-    <div className={styles.tableContainer}>
-      {data.preTableContent && (
-        <div className={styles.preTable}>
-          <p className={styles.preTableTitle}>
-            {data.preTableContent.title}
-            <span>
-              {getNameOfMonth()} {getCurrentYear()}
-            </span>
-          </p>
-        </div>
-      )}
-      <table className={styles.table}>
-        <tbody>
-          {data.tableContent.map((item, index) => (
-            <tr className={styles.tableRow} key={index}>
-              {item.row.map((subItem, subIndex) => (
-                <td className={styles.tableCell} key={subIndex}>
-                  {subItem.stats && (
-                    <div
-                      className={clsx(
-                        styles.cellContainer,
-                        subItem.stats.hasBackgroundColor === "true"
-                          ? styles.cellBackground
-                          : undefined
-                      )}
-                    >
-                      <h3 className={styles.cellTitle}>{subItem.stats.cellTitle}</h3>
-                      <p className={styles.cellSubTitle}>{subItem.stats.cellSubTitle}</p>
-                    </div>
-                  )}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <table className={styles.table}>
+      <tbody>
+        {data.tableContent.map((item, index) => (
+          <tr className={styles.tableRow} key={index}>
+            {item.row.map((subItem, subIndex) => (
+              <td className={styles.tableCell} key={subIndex}>
+                {subItem.board && (
+                  <div
+                    className={clsx(
+                      styles.cellContainer,
+                      subItem.stats.hasBackgroundColor === "true"
+                        ? styles.cellBackground
+                        : undefined
+                    )}
+                  >
+                    <NavbarLink
+                      label={subItem.board.image.label}
+                      link={subItem.board.image.link}
+                      isImageWrapped={true}
+                      imageSrcPath={subItem.board.image.srcPath}
+                      styleImage={styles.boardImage}
+                      imageWidth={70}
+                      imageHeight={70}
+                    />
+                    <NavbarLink
+                      label={subItem.board.body.label}
+                      link={subItem.board.body.link}
+                      styleLink={styles.boardLink}
+                    />
+                  </div>
+                )}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
