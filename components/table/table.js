3c78d0b4cefa8c1914d30a916069a77234f30496
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-03 10:41:00 -0400

    Made a basic structure for the Table component

--- components/table/table.js
+++ components/table/table.js
@@ -1,33 +1,46 @@
 import clsx from "clsx";
+import { getNameOfMonth, getCurrentYear } from "../../lib/functions/timeValues";
 import styles from "./table.module.scss";
 
 const Table = ({ data }) => {
   return (
-    <table className={styles.tableContainer}>
-      <tbody>
-        {data.map((item, index) => (
-          <tr className={styles.tableRow} key={index}>
-            {item.row.map((subItem, subIndex) => (
-              <td className={styles.tableCell} key={subIndex}>
-                {subItem.stats && (
-                  <div
-                    className={clsx(
-                      styles.cellContainer,
-                      subItem.stats.hasBackgroundColor === "true"
-                        ? styles.cellBackground
-                        : undefined
-                    )}
-                  >
-                    <h3 className={styles.cellTitle}>{subItem.stats.cellTitle}</h3>
-                    <p className={styles.cellSubTitle}>{subItem.stats.cellSubTitle}</p>
-                  </div>
-                )}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className={styles.tableContainer}>
+      {data.preTableContent && (
+        <div className={styles.preTable}>
+          <p>
+            {data.preTableContent.title}
+            <span>
+              {getNameOfMonth()} {getCurrentYear()}
+            </span>
+          </p>
+        </div>
+      )}
+      <table className={styles.table}>
+        <tbody>
+          {data.tableContent.map((item, index) => (
+            <tr className={styles.tableRow} key={index}>
+              {item.row.map((subItem, subIndex) => (
+                <td className={styles.tableCell} key={subIndex}>
+                  {subItem.stats && (
+                    <div
+                      className={clsx(
+                        styles.cellContainer,
+                        subItem.stats.hasBackgroundColor === "true"
+                          ? styles.cellBackground
+                          : undefined
+                      )}
+                    >
+                      <h3 className={styles.cellTitle}>{subItem.stats.cellTitle}</h3>
+                      <p className={styles.cellSubTitle}>{subItem.stats.cellSubTitle}</p>
+                    </div>
+                  )}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
