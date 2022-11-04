c3bb26f3f4b8d2497b11b0c34362871d9ea10374
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-04 14:32:51 -0400

    Removed List and LateralColumn from the services page

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,22 +1,10 @@
 import pageData from "../../data/pages/services/services.json";
 import Jumbotron from "../../components/jumbotron/jumbotron";
-import LateralColumn from "../../components/lateralColumn/lateralColumn";
-import List from "../../components/list/list";
-import utilStyles from "../../styles/utils.module.scss";
 
 const Services = () => {
   return (
     <>
       <Jumbotron data={pageData} />
-      <div className={utilStyles.bodyContainer}>
-        <LateralColumn data={pageData.lateralColumn1} />
-        <LateralColumn
-          data={pageData.lateralColumn2}
-          styleBackground={utilStyles.sectionBackground}
-        >
-          <List data={pageData.list1} />
-        </LateralColumn>
-      </div>
     </>
   );
 };
