1a0871be985a971468f665f84c32e8085e2eb7ac
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-07 10:10:22 -0500

    Refactored LateralColumn layout

--- components/lateralColumn/lateralColumn.js
+++ components/lateralColumn/lateralColumn.js
@@ -1,71 +1,10 @@
-import clsx from "clsx";
 import styles from "./lateralColumn.module.scss";
 
-const LateralColumn = ({ data, styleLeftColumnImage, children }) => {
+const LateralColumn = ({ leftColumn, rightColumn }) => {
   return (
     <div className={styles.lateralColumnContainer}>
-      <div className={styles.leftColumnContainer}>
-        {data.lateralColumn.leftColumn.header.title && (
-          <div
-            className={clsx(
-              styles.leftColumnHeader,
-              data.lateralColumn.leftColumn.header.hasImage === "true"
-                ? styleLeftColumnImage
-                : undefined
-            )}
-          >
-            <h1 className={styles.leftColumnTitle}>{data.lateralColumn.leftColumn.header.title}</h1>
-          </div>
-        )}
-        {data.lateralColumn.leftColumn.body?.text.map((item, index) => (
-          <p
-            className={clsx(
-              styles.leftColumnBodyText,
-              item.isLastSubText !== "true" ? styles.subTextMarginBottom : undefined
-            )}
-            key={index}
-          >
-            {item.subText}
-          </p>
-        ))}
-      </div>
-      <div className={styles.rightColumnContainer}>
-        {data.lateralColumn.rightColumn.header ? (
-          <>
-            {data.lateralColumn.rightColumn.header.title && (
-              <div className={styles.rightColumnHeader}>
-                <h1 className={styles.rightColumnTitle}>
-                  {data.lateralColumn.rightColumn.header.title}
-                </h1>
-                {data.lateralColumn.rightColumn.header.subTitle && (
-                  <h2 className={styles.rightColumnSubTitle}>
-                    {data.lateralColumn.rightColumn.header.subTitle}
-                  </h2>
-                )}
-              </div>
-            )}
-            {data.lateralColumn.rightColumn.body?.text ? (
-              <div className={styles.rightColumnBody}>
-                {data.lateralColumn.rightColumn.body.text.map((item, index) => (
-                  <p
-                    className={clsx(
-                      styles.rightColumnBodyText,
-                      item.isLastSubText !== "true" ? styles.subTextMarginBottom : undefined
-                    )}
-                    key={index}
-                  >
-                    {item.subText}
-                  </p>
-                ))}
-              </div>
-            ) : (
-              children
-            )}
-          </>
-        ) : (
-          children
-        )}
-      </div>
+      <div className={styles.leftColumnContainer}>{leftColumn}</div>
+      <div className={styles.rightColumnContainer}>{rightColumn}</div>
     </div>
   );
 };
