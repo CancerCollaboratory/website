86a6404d43ff438ffef714d427d5329830fd2729
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-07 09:02:00 -0500

    Restored deleted file content

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,29 @@
-const Home = () => {
+import { getPageData } from "../lib/pages.js";
+import { attributesToProps } from "html-react-parser";
+
+const insertLink = {
+  replace: domNode => {
+    if (domNode.attribs && domNode.name === "a") {
+      const props = attributesToProps(domNode.attribs);
+      return (
+        <a target="_blank" {...props}>
+          {domNode.children[0].data}
+        </a>
+      );
+    }
+  },
+};
+
+export async function getStaticProps() {
+  const pageData = getPageData("home");
+  return {
+    props: {
+      pageData,
+    },
+  };
+}
+
+const Home = ({ pageData }) => {
   return <></>;
 };
 
