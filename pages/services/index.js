ea874037b37e2e881ed9640a83b7a452a8af01ec
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-07 17:03:39 -0500

    Used components

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,10 +1,14 @@
-import pageData from "../../data/pages/services/services.json";
 import Jumbotron from "../../components/jumbotron/jumbotron";
+import LeftColumn from "../../components/leftColumn/leftColumn";
+import RightColumn from "../../components/rightColumn/rightColumn";
+import LateralColumn from "../../components/lateralColumn/lateralColumn";
+import pageData from "../../data/pages/services/services.json";
 
 const Services = () => {
   return (
     <>
       <Jumbotron data={pageData} />
+      <LateralColumn leftColumn={<LeftColumn />} rightColumn={<RightColumn />} />
     </>
   );
 };
