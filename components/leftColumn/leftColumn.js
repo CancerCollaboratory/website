e1e03dd68d6b94ede94733b5f2aac6be553d1bf3
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-08 12:43:03 -0500

    Built LeftColumn

--- components/leftColumn/leftColumn.js
+++ components/leftColumn/leftColumn.js
@@ -1,7 +1,22 @@
+import clsx from "clsx";
 import styles from "./leftColumn.module.scss";
 
-const LeftColumn = () => {
-  return <></>;
+const LeftColumn = ({ title, styleImage, body, children }) => {
+  return (
+    <>
+      {title && (
+        <div className={clsx(styles.header, styleImage)}>
+          <h1 className={styles.title}>{title}</h1>
+        </div>
+      )}
+      {body?.map((item, index) => (
+        <p className={styles.bodyText} key={index}>
+          {item.text}
+        </p>
+      ))}
+      {children}
+    </>
+  );
 };
 
 export default LeftColumn;
