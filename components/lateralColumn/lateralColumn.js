2b73cf497c6eb634746f71f372938b3919197fa4
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-08 14:58:11 -0500

    Added a responsive bottom border

--- components/lateralColumn/lateralColumn.js
+++ components/lateralColumn/lateralColumn.js
@@ -3,10 +3,13 @@ import styles from "./lateralColumn.module.scss";
 
 const LateralColumn = ({ leftColumn, rightColumn }) => {
   return (
-    <div className={styles.lateralColumnContainer}>
-      <Column>{leftColumn}</Column>
-      <Column>{rightColumn}</Column>
-    </div>
+    <>
+      <div className={styles.lateralColumnContainer}>
+        <Column>{leftColumn}</Column>
+        <Column>{rightColumn}</Column>
+      </div>
+      <div className={styles.bottomBorder}></div>
+    </>
   );
 };
 
