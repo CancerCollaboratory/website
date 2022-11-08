03f91b01974372d6edeebb18b3ea07512a1eb468
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-08 12:41:42 -0500

    Built RightColumn

--- components/rightColumn/rightColumn.js
+++ components/rightColumn/rightColumn.js
@@ -1,7 +1,22 @@
 import styles from "./rightColumn.module.scss";
 
-const RightColumn = () => {
-  return <></>;
+const RightColumn = ({ title, subTitle, body, children }) => {
+  return (
+    <>
+      {title && (
+        <>
+          <h1 className={styles.title}>{title}</h1>
+          {subTitle && <h2 className={styles.subTitle}>{subTitle}</h2>}
+        </>
+      )}
+      {body?.map((item, index) => (
+        <p className={styles.bodyText} key={index}>
+          {item.text}
+        </p>
+      ))}
+      {children}
+    </>
+  );
 };
 
 export default RightColumn;
