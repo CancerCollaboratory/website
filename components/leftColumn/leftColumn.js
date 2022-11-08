ba1fa25a27bffe9acb0dd61e297996ba7901f734
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-08 13:26:29 -0500

    Made a minor change

--- components/leftColumn/leftColumn.js
+++ components/leftColumn/leftColumn.js
@@ -4,11 +4,7 @@ import styles from "./leftColumn.module.scss";
 const LeftColumn = ({ title, styleImage, body, children }) => {
   return (
     <>
-      {title && (
-        <div className={clsx(styles.header, styleImage)}>
-          <h1 className={styles.title}>{title}</h1>
-        </div>
-      )}
+      {title && <h1 className={clsx(styles.title, styleImage)}>{title}</h1>}
       {body?.map((item, index) => (
         <p className={styles.bodyText} key={index}>
           {item.text}
