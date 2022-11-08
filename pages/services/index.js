ae8585e4ed44794a1c3a8c6008b03c7594018de8
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-08 12:40:21 -0500

    Used components

--- pages/services/index.js
+++ pages/services/index.js
@@ -2,13 +2,44 @@ import Jumbotron from "../../components/jumbotron/jumbotron";
 import LeftColumn from "../../components/leftColumn/leftColumn";
 import RightColumn from "../../components/rightColumn/rightColumn";
 import LateralColumn from "../../components/lateralColumn/lateralColumn";
+import List from "../../components/list/list";
 import pageData from "../../data/pages/services/services.json";
+import styles from "./services.module.scss";
+
+const latCol1Left = pageData.lateralColumn1.leftColumn;
+const latCol2Left = pageData.lateralColumn2.leftColumn;
+const latCol1Right = pageData.lateralColumn1.rightColumn;
+const list = pageData.list;
 
 const Services = () => {
   return (
     <>
+      {console.log(pageData)}
       <Jumbotron data={pageData} />
-      <LateralColumn leftColumn={<LeftColumn />} rightColumn={<RightColumn />} />
+      <LateralColumn
+        leftColumn={
+          <LeftColumn
+            title={latCol1Left.title}
+            styleImage={styles.latCol1LeftImage}
+            body={latCol1Left.body}
+          />
+        }
+        rightColumn={<RightColumn title={latCol1Right.title} subTitle={latCol1Right.subTitle} />}
+      />
+      <LateralColumn
+        leftColumn={
+          <LeftColumn
+            title={latCol2Left.title}
+            styleImage={styles.latCol2LeftImage}
+            body={latCol2Left.body}
+          />
+        }
+        rightColumn={
+          <RightColumn>
+            <List title={list.title} body={list.body} />
+          </RightColumn>
+        }
+      />
     </>
   );
 };
