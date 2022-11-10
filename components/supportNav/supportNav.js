7e96ed85fc09a58d31467e30fea59f120b321400
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-10 11:37:13 -0500

    Removed a fragment

--- components/supportNav/supportNav.js
+++ components/supportNav/supportNav.js
@@ -2,20 +2,17 @@ import NavbarLink from "../../components/navbarLink/navbarLink";
 import navData from "../../data/components/navbar/navbar.json";
 import styles from "./supportNav.module.scss";
 
-const SupportNav = ({ children }) => {
+const SupportNav = () => {
   const supportItemIndex = navData.navbarItems.findIndex(item => item.link.includes("/support"));
 
   return (
-    <>
-      <ul className={styles.navContainer}>
-        {navData.navbarItems[supportItemIndex].navbarSubMenuItems.map((item, index) => (
-          <li key={index}>
-            <NavbarLink label={item.label} link={item.link} />
-          </li>
-        ))}
-      </ul>
-      {children}
-    </>
+    <ul className={styles.navContainer}>
+      {navData.navbarItems[supportItemIndex].navbarSubMenuItems.map((item, index) => (
+        <li key={index}>
+          <NavbarLink label={item.label} link={item.link} />
+        </li>
+      ))}
+    </ul>
   );
 };
 
