309f2cd8c78f3c59aff8094b962d22de6d55842c
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-10 16:16:17 -0500

    Built Table

---
+++ components/table/table.js
@@ -0,0 +1,34 @@
+import { renderData } from "../../lib/functions/renderData";
+import styles from "./table.module.scss";
+
+const Table = ({ tableTitle, tableBody }) => {
+  return (
+    <>
+      {tableTitle && <p className={styles.tableTitle}>{tableTitle}</p>}
+      <table className={styles.table}>
+        <tbody>
+          {tableBody.map((item, index) => (
+            <tr key={index}>
+              {item.columns.map((column, subIndex) => (
+                <td className={styles.tableCellTitle} key={subIndex}>
+                  {column.title &&
+                    renderData(
+                      column.title,
+                      column.cellHead.isLinkExternal === "true"
+                        ? insertLinkExternal
+                        : insertLinkInternal
+                    )}
+                  {column.content && (
+                    <div className={styles.tableCellContent}>{renderData(column.content)}</div>
+                  )}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Table;
