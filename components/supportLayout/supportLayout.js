27e0f8b6adf82e9e5d57a4907d432f61484feb50
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-11 14:00:56 -0500

    Added SupportSidebar to SupportLayout

--- components/supportLayout/supportLayout.js
+++ components/supportLayout/supportLayout.js
@@ -1,10 +1,12 @@
 import Navbar from "../navbar/navbar";
+import SupportSidebar from "../supportSidebar/supportSidebar";
 import Footer from "../footer/footer";
 
 const SupportLayout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <SupportSidebar />
       {children}
       <Footer />
     </>
