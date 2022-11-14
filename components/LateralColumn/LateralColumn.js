17c7485b032b072f3dd63f775f86792cb1e1dde2
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-14 09:37:35 -0500

    Added a prop to LateralColumn

--- components/LateralColumn/LateralColumn.js
+++ components/LateralColumn/LateralColumn.js
@@ -1,9 +1,10 @@
+import clsx from "clsx";
 import Column from "../column/column";
 import styles from "./lateralColumn.module.scss";
 
-const LateralColumn = ({ leftColumn, rightColumn }) => {
+const LateralColumn = ({ leftColumn, rightColumn, styleBackground }) => {
   return (
-    <div className={styles.lateralColumnContainer}>
+    <div className={clsx(styles.lateralColumnContainer, styleBackground)}>
       {leftColumn && <Column styleColumn={styles.leftColumn}>{leftColumn}</Column>}
       {rightColumn && <Column styleColumn={styles.rightColumn}>{rightColumn}</Column>}
     </div>
