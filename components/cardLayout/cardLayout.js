4a4a93d3afb7f160848733edab8282f0d58b8cb1
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-14 16:52:15 -0500

    Used global util styles

--- components/cardLayout/cardLayout.js
+++ components/cardLayout/cardLayout.js
@@ -1,14 +1,18 @@
+import clsx from "clsx";
 import ResourceCard from "../resourceCard/resourceCard";
+import utilStyles from "../../styles/utils.module.scss";
 import styles from "./cardLayout.module.scss";
 
 const CardLayout = ({ cards }) => {
   return (
-    <div className={styles.cardLayout}>
+    <div className={clsx(styles.cardLayout, utilStyles.componentContainer)}>
       {cards.map((card, index) => (
         <ResourceCard
           title={card.title}
           isTitleLinkExternal={card.isTitleLinkExternal}
           bodyText={card.bodyText}
+          imageSrcPath={card.imageSrcPath}
+          imageAlt={card.imageAlt}
           key={index}
         />
       ))}
