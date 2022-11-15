71b6345b394e14798d1f23741d66d1f6170a611a
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-15 17:21:36 -0500

    Created ResourceSection component

---
+++ components/resourceSection/resourceSection.js
@@ -0,0 +1,7 @@
+import styles from "./resourceSection.module.scss";
+
+const ResourceSection = () => {
+  return <div className={styles.sectionContainer}></div>;
+};
+
+export default ResourceSection;
