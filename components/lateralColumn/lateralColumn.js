04f66f5b5b95aa4b399c7f6f38610cf07b02836f
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-15 14:12:04 -0500

    Removed the use of global util styles

--- components/lateralColumn/lateralColumn.js
+++ components/lateralColumn/lateralColumn.js
@@ -1,17 +1,10 @@
 import clsx from "clsx";
 import Column from "../column/column";
-import utilStyles from "../../styles/utils.module.scss";
 import styles from "./lateralColumn.module.scss";
 
 const LateralColumn = ({ leftColumn, rightColumn, styleBackground }) => {
   return (
-    <div
-      className={clsx(
-        styles.lateralColumnContainer,
-        utilStyles.componentContainer,
-        styleBackground
-      )}
-    >
+    <div className={clsx(styles.lateralColumnContainer, styleBackground)}>
       {leftColumn && <Column styleColumn={styles.leftColumn}>{leftColumn}</Column>}
       {rightColumn && <Column styleColumn={styles.rightColumn}>{rightColumn}</Column>}
     </div>
