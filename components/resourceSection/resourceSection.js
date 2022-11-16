c53bc25fc9dbf30c7f3284dc1702eb2f8530615b
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-15 20:43:54 -0500

    Added some props to ResourceSection

--- components/resourceSection/resourceSection.js
+++ components/resourceSection/resourceSection.js
@@ -1,7 +1,21 @@
+import { renderData } from "../../lib/functions/renderData";
+import { insertLinkInternal, insertLinkExternal } from "../../lib/functions/insertLink";
 import styles from "./resourceSection.module.scss";
 
-const ResourceSection = () => {
-  return <div className={styles.sectionContainer}></div>;
+const ResourceSection = ({ title, bodyText, isBodyTextLinkExternal }) => {
+  return (
+    <div className={styles.sectionContainer}>
+      {title && <h1>{title}</h1>}
+      {bodyText && (
+        <div>
+          {renderData(
+            bodyText,
+            isBodyTextLinkExternal === "ture" ? insertLinkExternal : insertLinkInternal
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ResourceSection;
