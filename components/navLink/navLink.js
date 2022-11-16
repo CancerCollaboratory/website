509752e985df94fc8d604bf38fbf186af6ca6d9b
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-16 17:05:01 -0500

    Used an arrow icon

--- components/navLink/navLink.js
+++ components/navLink/navLink.js
@@ -1,5 +1,7 @@
 import clsx from "clsx";
 import Link from "next/link";
+import Image from "next/future/image";
+import IconArrow from "../../public/assets/navLink/icon-arrow-red.svg";
 import styles from "./navLink.module.scss";
 
 const NavLink = ({ links }) => {
@@ -9,11 +11,19 @@ const NavLink = ({ links }) => {
         <li
           className={clsx(
             styles.label,
-            item.isLastLink !== "true" ? styles.borderRight : undefined
+            item.isLastLink !== "true" ? clsx(styles.borderRight, styles.marginBottom) : undefined
           )}
           key={index}
         >
           {item.label && item.link && <Link href={item.link}>{item.label}</Link>}
+          <Image
+            src={IconArrow}
+            alt={"Icon"}
+            className={styles.iconArrow}
+            width={11}
+            height={11}
+            quality={100}
+          />
         </li>
       ))}
     </ul>
