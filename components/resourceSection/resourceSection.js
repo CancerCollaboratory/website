651107d814dfecca21001945310d16b8ebe72e01
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-17 10:28:38 -0500

    Used a util style in ResourceSection

--- components/resourceSection/resourceSection.js
+++ components/resourceSection/resourceSection.js
@@ -4,10 +4,17 @@ import NavLink from "../navLink/navLink";
 import { renderData } from "../../lib/functions/renderData";
 import { insertLinkInternal, insertLinkExternal } from "../../lib/functions/insertLink";
 import styles from "./resourceSection.module.scss";
+import utilStyles from "../../styles/utils.module.scss";
 
 const ResourceSection = ({ title, cards, caption, isCaptionLinkExternal, navLinks }) => {
   return (
-    <div className={clsx(styles.sectionContainer, caption && styles.background)}>
+    <div
+      className={clsx(
+        styles.sectionContainer,
+        utilStyles.outerContainer,
+        caption && styles.background
+      )}
+    >
       {title && <h1 className={styles.title}>{title}</h1>}
       {cards && <CardLayout cards={cards} />}
       {caption && (
