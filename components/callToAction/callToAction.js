2e133b85fd7d20073e2dab9eb727bc62255958f2
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-17 16:15:39 -0500

    Added some parts to CallToAction

--- components/callToAction/callToAction.js
+++ components/callToAction/callToAction.js
@@ -1,7 +1,16 @@
+import Button from "../button/button";
 import styles from "./callToAction.module.scss";
 
-const CallToAction = () => {
-  return <></>;
+const CallToAction = ({ title, bodyText, button }) => {
+  return (
+    <div className={styles.ctaContainer}>
+      {title && <h2 className={styles.title}>{title}</h2>}
+      {bodyText && <p className={styles.bodyText}>{bodyText}</p>}
+      {buttonData && (
+        <Button title={button.title} link={button.link} isLinkExternal={button.isLinkExternal} />
+      )}
+    </div>
+  );
 };
 
 export default CallToAction;
