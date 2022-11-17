b58271df372043e325044d19f5777aa34fd0c1b6
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-17 10:20:29 -0500

    Used a util style

--- components/lateralColumn/lateralColumn.js
+++ components/lateralColumn/lateralColumn.js
@@ -1,10 +1,13 @@
 import clsx from "clsx";
 import Column from "../column/column";
 import styles from "./lateralColumn.module.scss";
+import utilStyles from "../../styles/utils.module.scss";
 
 const LateralColumn = ({ leftColumn, rightColumn, styleBackground }) => {
   return (
-    <div className={clsx(styles.lateralColumnContainer, styleBackground)}>
+    <div
+      className={clsx(styles.lateralColumnContainer, utilStyles.outerContainer, styleBackground)}
+    >
       {leftColumn && <Column styleColumn={styles.leftColumn}>{leftColumn}</Column>}
       {rightColumn && <Column styleColumn={styles.rightColumn}>{rightColumn}</Column>}
     </div>
