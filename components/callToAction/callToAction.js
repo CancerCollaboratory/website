0c26787995b27691a24482dc2fe8a6264a3f5f10
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-17 16:46:11 -0500

    Used util styles

--- components/callToAction/callToAction.js
+++ components/callToAction/callToAction.js
@@ -1,9 +1,10 @@
+import clsx from "clsx";
 import Button from "../button/button";
 import styles from "./callToAction.module.scss";
 
-const CallToAction = ({ title, bodyText, button }) => {
+const CallToAction = ({ title, bodyText, button, styleBackground }) => {
   return (
-    <div className={styles.ctaContainer}>
+    <div className={clsx(styles.container, styleBackground)}>
       {title && <h2 className={styles.title}>{title}</h2>}
       {bodyText && <p className={styles.bodyText}>{bodyText}</p>}
       {buttonData && (
