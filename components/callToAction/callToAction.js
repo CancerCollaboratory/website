efc82064e3bb25402072e0109da5dd1fd20f6beb
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-18 15:44:42 -0500

    Built a cta component

--- components/callToAction/callToAction.js
+++ components/callToAction/callToAction.js
@@ -1,14 +1,52 @@
 import clsx from "clsx";
 import Button from "../button/button";
+import Image from "next/future/image";
+import utilStyles from "../../styles/utils.module.scss";
 import styles from "./callToAction.module.scss";
 
-const CallToAction = ({ title, bodyText, button, styleBackground }) => {
+const CallToAction = ({ title, imageSrcPath, imageAlt, bodyText, buttonData, styleBackground }) => {
   return (
-    <div className={clsx(styles.container, styleBackground)}>
-      {title && <h2 className={styles.title}>{title}</h2>}
-      {bodyText && <p className={styles.bodyText}>{bodyText}</p>}
-      {buttonData && (
-        <Button title={button.title} link={button.link} isLinkExternal={button.isLinkExternal} />
+    <div className={clsx(styles.container, styleBackground, utilStyles.outerContainer)}>
+      {title && (
+        <div className={styles.titleContainer}>
+          {imageSrcPath && (
+            <Image
+              src={imageSrcPath}
+              alt={imageAlt}
+              className={styles.image}
+              width={25}
+              height={25}
+              quality={100}
+            />
+          )}
+          <h2 className={styles.title}>{title}</h2>
+        </div>
+      )}
+      {(bodyText || buttonData) && (
+        <div className={styles.bodyContainer}>
+          {bodyText && (
+            <p
+              className={clsx(
+                styles.bodyText,
+                !title && !buttonData ? styles.isolateText : undefined
+              )}
+            >
+              {bodyText}
+            </p>
+          )}
+          {buttonData && (
+            <div className={styles.buttonContainer}>
+              {buttonData.map((button, index) => (
+                <Button
+                  label={button.label}
+                  link={button.link}
+                  isLinkExternal={button.isLinkExternal}
+                  key={index}
+                />
+              ))}
+            </div>
+          )}
+        </div>
       )}
     </div>
   );
