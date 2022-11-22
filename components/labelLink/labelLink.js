0d5afa8d4bf8bd84c9ac86d796a147dcdf5d9d4f
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-22 17:27:44 -0500

    Made a minor change

--- components/labelLink/labelLink.js
+++ components/labelLink/labelLink.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 const LabelLink = ({ label, link, isLinkExternal, styleLabel, styleWrap }) => {
   const labelContainer = styleWrap ? <div className={styleWrap}>{label}</div> : label;
 
-  return !label || !link ? null : isLinkExternal === "true" ? (
+  return !label || !link ? null : isLinkExternal ? (
     <a className={styleLabel} href={link} target="_blank" rel="noreferrer">
       {labelContainer}
     </a>
