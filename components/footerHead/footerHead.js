97665f8c1059ec79854b1d6215fa61f997e4d9b3
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-22 16:11:10 -0500

    Removed unneeded code

--- components/footerHead/footerHead.js
+++ components/footerHead/footerHead.js
@@ -6,41 +6,37 @@ import { getCurrentYear } from "../../lib/functions/timeValue";
 import styles from "../footer/footer.module.scss";
 
 const FooterHead = ({ headItem }) => {
-  return (
-    <>
-      {headItem.logo ? (
-        <div className={styles.footerLogoContainer}>
-          <ImageLink link={headItem.logo.link} isLinkExternal={"true"}>
-            <Image
-              src={headItem.logo.srcPath}
-              alt={headItem.logo.label}
-              className={styles.footerLogo}
-              quality={100}
-              width={77}
-              height={56}
+  return headItem.logo ? (
+    <div className={styles.footerLogoContainer}>
+      <ImageLink link={headItem.logo.link} isLinkExternal={"true"}>
+        <Image
+          src={headItem.logo.srcPath}
+          alt={headItem.logo.label}
+          className={styles.footerLogo}
+          quality={100}
+          width={77}
+          height={56}
+        />
+      </ImageLink>
+    </div>
+  ) : (
+    <div className={styles.headItemContainer}>
+      <p className={styles.headItemLabel}>
+        © <span>{getCurrentYear()}</span> {headItem.label}
+      </p>
+      <div className={styles.headLinksContainer}>
+        {headItem.links.map((linkItem, index) => (
+          <React.Fragment key={index}>
+            <LabelLink
+              label={linkItem.label}
+              link={linkItem.link}
+              isLinkExternal={linkItem.isLinkExternal}
             />
-          </ImageLink>
-        </div>
-      ) : (
-        <div className={styles.headItemContainer}>
-          <p className={styles.headItemLabel}>
-            © <span>{getCurrentYear()}</span> {headItem.label}
-          </p>
-          <div className={styles.headLinksContainer}>
-            {headItem.links.map((linkItem, index) => (
-              <React.Fragment key={index}>
-                <LabelLink
-                  label={linkItem.label}
-                  link={linkItem.link}
-                  isLinkExternal={linkItem.isLinkExternal}
-                />
-                {!linkItem.lastLink && <div className={styles.verticalLine}></div>}
-              </React.Fragment>
-            ))}
-          </div>
-        </div>
-      )}
-    </>
+            {!linkItem.lastLink && <div className={styles.verticalLine}></div>}
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
   );
 };
 
