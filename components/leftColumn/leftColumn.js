995cf92d51c25b51e0440598687890c9ef4346d7
Dhyey Patel <97138744+dhyeypatell@users.noreply.github.com>
2022-11-22 11:36:33 -0500

    #46 - Center The Title In LeftColumn Component
    * Modified styling to center the title in LeftColumn component
    
    * Improved the styling of LeftColumn component
    
    * Edited LateralColumn data in services.json

--- components/leftColumn/leftColumn.js
+++ components/leftColumn/leftColumn.js
@@ -1,10 +1,24 @@
-import clsx from "clsx";
+import Image from "next/future/image";
 import styles from "./leftColumn.module.scss";
 
-const LeftColumn = ({ title, styleTitleImage, body, children }) => {
+const LeftColumn = ({ title, imageSrcPath, imageAlt, body, children }) => {
   return (
     <>
-      {title && <h2 className={clsx(styles.title, styleTitleImage)}>{title}</h2>}
+      {(title || imageSrcPath) && (
+        <div className={styles.head}>
+          {imageSrcPath && (
+            <Image
+              src={imageSrcPath}
+              alt={imageAlt}
+              className={styles.image}
+              width={62}
+              height={62}
+              quality={100}
+            />
+          )}
+          {title && <h2 className={styles.title}>{title}</h2>}
+        </div>
+      )}
       {body?.map(
         (item, index) =>
           item.text && (
