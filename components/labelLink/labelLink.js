9345eed819bac49df5e033957aea0e2fdd9d607c
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-22 17:26:46 -0500

    Updated conditional rendering in LabelLink

--- components/labelLink/labelLink.js
+++ components/labelLink/labelLink.js
@@ -3,18 +3,14 @@ import Link from "next/link";
 const LabelLink = ({ label, link, isLinkExternal, styleLabel, styleWrap }) => {
   const labelContainer = styleWrap ? <div className={styleWrap}>{label}</div> : label;
 
-  return (
-    label &&
-    link &&
-    (isLinkExternal === "true" ? (
-      <a className={styleLabel} href={link} target="_blank" rel="noreferrer">
-        {labelContainer}
-      </a>
-    ) : (
-      <Link href={link}>
-        <a className={styleLabel}>{labelContainer}</a>
-      </Link>
-    ))
+  return !label || !link ? null : isLinkExternal === "true" ? (
+    <a className={styleLabel} href={link} target="_blank" rel="noreferrer">
+      {labelContainer}
+    </a>
+  ) : (
+    <Link href={link}>
+      <a className={styleLabel}>{labelContainer}</a>
+    </Link>
   );
 };
 
