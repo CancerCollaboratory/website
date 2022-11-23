d2790d8dbc669ee7710f8643a7d079724f89adba
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-23 14:02:05 -0500

    Restructured CustomLink

--- components/customLink/customLink.js
+++ components/customLink/customLink.js
@@ -1,31 +1,22 @@
 import Link from "next/link";
 
 const CustomLink = ({ label, link, isLinkExternal, styleLabel, styleWrap, children }) => {
-  const labelContainer = styleWrap ? <div className={styleWrap}>{label}</div> : label;
+  if (!link || (!children && !label) || (children && label)) {
+    return null;
+  } else {
+    const labelContainer = styleWrap ? <div className={styleWrap}>{label}</div> : label;
+    const content = children || labelContainer;
 
-  return !link ? null : label && !children ? (
-    isLinkExternal ? (
+    return isLinkExternal ? (
       <a className={styleLabel} href={link} target="_blank" rel="noreferrer">
-        {labelContainer}
+        {content}
       </a>
     ) : (
       <Link href={link}>
-        <a className={styleLabel}>{labelContainer}</a>
+        <a className={styleLabel}>{content}</a>
       </Link>
-    )
-  ) : (
-    !label &&
-    children &&
-    (isLinkExternal ? (
-      <a href={link} target="_blank" rel="noreferrer">
-        {children}
-      </a>
-    ) : (
-      <Link href={link}>
-        <a>{children}</a>
-      </Link>
-    ))
-  );
+    );
+  }
 };
 
 export default CustomLink;
