94546e12f040cc933373d9f59cd733ee8d540977
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-23 14:32:13 -0500

    Edited a comment in CustomLink

--- components/customLink/customLink.js
+++ components/customLink/customLink.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-// To use CustomLink, pass either the label prop OR children prop but not both
+// Must pass a link prop and either a label prop OR children prop but not both
 const CustomLink = ({ label, link, isLinkExternal, styleLabel, styleWrap, children }) => {
   if (!link || (!children && !label) || (children && label)) {
     return null;
