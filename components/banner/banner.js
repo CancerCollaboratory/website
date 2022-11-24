55c16c15fea5f23bd510ba970f49f0c93a9729b3
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-24 15:04:34 -0500

    Restructured Banner

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,36 +1,38 @@
+import clsx from "clsx";
 import CloseButton from "../closeButton/clossButton";
 import { useState } from "react";
 import { renderData } from "../../lib/functions/renderData";
 import { insertLinkInternal, insertLinkExternal } from "../../lib/functions/insertLink";
+import utilStyles from "../../styles/utils.module.scss";
 import styles from "./banner.module.scss";
 
 const Banner = ({ title, description, isDescriptionLinkExternal, children }) => {
-  const [showBanner, setShowBanner] = useState(true);
+  if ((!title && !description) || (title && !description && !children)) {
+    return null;
+  } else {
+    const [showBanner, setShowBanner] = useState(true);
 
-  return (
-    showBanner && (
-      <div className={styles.outerContainer}>
-        {(title || description) && (
-          <>
+    return (
+      showBanner && (
+        <div className={styles.container}>
+          <div className={clsx(styles.wrapContent, utilStyles.defaultContainerWidth)}>
             <div className={styles.content}>
               {title && <h1 className={styles.title}>{title}</h1>}
-              {(description || children) && (
-                <div className={styles.innerContainer}>
-                  {description &&
-                    renderData(
-                      description,
-                      isDescriptionLinkExternal ? insertLinkExternal : insertLinkInternal
-                    )}
-                  {children}
-                </div>
-              )}
+              <div className={styles.block}>
+                {description &&
+                  renderData(
+                    description,
+                    isDescriptionLinkExternal ? insertLinkExternal : insertLinkInternal
+                  )}
+                {children}
+              </div>
             </div>
             <CloseButton onClick={() => setShowBanner(false)} />
-          </>
-        )}
-      </div>
-    )
-  );
+          </div>
+        </div>
+      )
+    );
+  }
 };
 
 export default Banner;
