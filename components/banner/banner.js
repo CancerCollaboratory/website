1e4fe36b8a05e591d8901e757cb5070efddb7a69
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-24 13:12:05 -0500

    Built Banner

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,7 +1,36 @@
+import CloseButton from "../closeButton/clossButton";
+import { useState } from "react";
+import { renderData } from "../../lib/functions/renderData";
+import { insertLinkInternal, insertLinkExternal } from "../../lib/functions/insertLink";
 import styles from "./banner.module.scss";
 
-const Banner = () => {
-  return <></>;
+const Banner = ({ title, description, isDescriptionLinkExternal, children }) => {
+  const [showBanner, setShowBanner] = useState(true);
+
+  return (
+    showBanner && (
+      <div className={styles.outerContainer}>
+        {(title || description) && (
+          <>
+            <div className={styles.content}>
+              {title && <h1 className={styles.title}>{title}</h1>}
+              {(description || children) && (
+                <div className={styles.innerContainer}>
+                  {description &&
+                    renderData(
+                      description,
+                      isDescriptionLinkExternal ? insertLinkExternal : insertLinkInternal
+                    )}
+                  {children}
+                </div>
+              )}
+            </div>
+            <CloseButton onClick={() => setShowBanner(false)} />
+          </>
+        )}
+      </div>
+    )
+  );
 };
 
 export default Banner;
