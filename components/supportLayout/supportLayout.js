f743544bc79bb9608d80c7958ad37cc9af2a056f
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-24 18:10:33 -0500

    Restructured SupportLayout

--- components/supportLayout/supportLayout.js
+++ components/supportLayout/supportLayout.js
@@ -1,4 +1,3 @@
-import BannerLayout from "../bannerLayout/bannerLayout";
 import Navbar from "../navbar/navbar";
 import SupportSidebar from "../supportSidebar/supportSidebar";
 import Footer from "../footer/footer";
@@ -6,7 +5,6 @@ import Footer from "../footer/footer";
 const SupportLayout = ({ children }) => {
   return (
     <>
-      <BannerLayout />
       <Navbar />
       <SupportSidebar />
       {children}
