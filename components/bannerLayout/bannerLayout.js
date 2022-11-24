705df5772d6789becaa57ca10cb527cb2f0b18dc
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-24 11:21:44 -0500

    Built BannerLayout component

---
+++ components/bannerLayout/bannerLayout.js
@@ -0,0 +1,25 @@
+import Banner from "../banner/banner";
+import CustomLink from "../customLink/customLink";
+import bannerData from "../../data/components/banner/banner.json";
+
+const BannerLayout = () => {
+  const banner = bannerData.banner;
+
+  return (
+    <Banner
+      title={banner.title}
+      description={banner.description}
+      isDescriptionLinkExternal={banner.isDescriptionLinkExternal}
+    >
+      {banner.linkLabel && banner.linkTarget && (
+        <CustomLink
+          label={banner.linkLabel}
+          link={banner.linkTarget}
+          isLinkExternal={banner.isLinkTargetExternal}
+        />
+      )}
+    </Banner>
+  );
+};
+
+export default BannerLayout;
