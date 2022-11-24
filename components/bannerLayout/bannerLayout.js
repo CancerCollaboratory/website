35ce2e45a0b2c885caeb3f433586d0b20a286d78
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-24 18:06:48 -0500

    Restructured BannerLayout

--- components/bannerLayout/bannerLayout.js
+++ components/bannerLayout/bannerLayout.js
@@ -2,26 +2,29 @@ import Banner from "../banner/banner";
 import CustomLink from "../customLink/customLink";
 import bannerData from "../../data/components/banner/banner.json";
 
-const BannerLayout = () => {
+const BannerLayout = ({ children }) => {
   const banner = bannerData.banner;
 
   if (banner.isActive !== true) {
-    return null;
+    return children;
   } else {
     return (
-      <Banner
-        title={banner.title}
-        description={banner.description}
-        isDescriptionLinkExternal={banner.isDescriptionLinkExternal}
-      >
-        {banner.linkLabel && banner.linkTarget && (
-          <CustomLink
-            label={banner.linkLabel}
-            link={banner.linkTarget}
-            isLinkExternal={banner.isLinkTargetExternal}
-          />
-        )}
-      </Banner>
+      <>
+        <Banner
+          title={banner.title}
+          description={banner.description}
+          isDescriptionLinkExternal={banner.isDescriptionLinkExternal}
+        >
+          {banner.linkLabel && banner.linkTarget && (
+            <CustomLink
+              label={banner.linkLabel}
+              link={banner.linkTarget}
+              isLinkExternal={banner.isLinkTargetExternal}
+            />
+          )}
+        </Banner>
+        {children}
+      </>
     );
   }
 };
