2707d9215cc1274cffc10f81617cc634a0c7bc60
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-24 17:40:21 -0500

    Improved BannerLayout structure

--- components/bannerLayout/bannerLayout.js
+++ components/bannerLayout/bannerLayout.js
@@ -5,21 +5,25 @@ import bannerData from "../../data/components/banner/banner.json";
 const BannerLayout = () => {
   const banner = bannerData.banner;
 
-  return (
-    <Banner
-      title={banner.title}
-      description={banner.description}
-      isDescriptionLinkExternal={banner.isDescriptionLinkExternal}
-    >
-      {banner.linkLabel && banner.linkTarget && (
-        <CustomLink
-          label={banner.linkLabel}
-          link={banner.linkTarget}
-          isLinkExternal={banner.isLinkTargetExternal}
-        />
-      )}
-    </Banner>
-  );
+  if (banner.isActive !== true) {
+    return null;
+  } else {
+    return (
+      <Banner
+        title={banner.title}
+        description={banner.description}
+        isDescriptionLinkExternal={banner.isDescriptionLinkExternal}
+      >
+        {banner.linkLabel && banner.linkTarget && (
+          <CustomLink
+            label={banner.linkLabel}
+            link={banner.linkTarget}
+            isLinkExternal={banner.isLinkTargetExternal}
+          />
+        )}
+      </Banner>
+    );
+  }
 };
 
 export default BannerLayout;
