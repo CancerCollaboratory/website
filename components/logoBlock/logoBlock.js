08720632f59fcc6600371c3def76656b2e5697f1
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-25 11:56:22 -0500

    Built LogoBlock

--- components/logoBlock/logoBlock.js
+++ components/logoBlock/logoBlock.js
@@ -1,5 +1,25 @@
-const LogoBlock = () => {
-  return <></>;
+import CustomLink from "../customLink/customLink";
+import Image from "next/future/image";
+import styles from "./logoBlock.module.scss";
+
+const LogoBlock = ({ block }) => {
+  const logo = block.logo;
+  if (!logo || !logo.link || !logo.srcPath || !logo.alt) {
+    return null;
+  } else {
+    return (
+      <CustomLink link={logo.link} isLinkExternal={logo.isLinkExternal}>
+        <Image
+          src={logo.srcPath}
+          alt={logo.alt}
+          className={styles.logo}
+          quality={100}
+          width={90}
+          height={70}
+        />
+      </CustomLink>
+    );
+  }
 };
 
 export default LogoBlock;
