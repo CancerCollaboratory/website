4fb9dea61e85e10579a4cbb5332c08c89110a039
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-25 13:03:38 -0500

    Edited a div tag in CloseButton

--- components/closeButton/clossButton.js
+++ components/closeButton/clossButton.js
@@ -1,7 +1,7 @@
 import styles from "./closeButton.module.scss";
 
 const CloseButton = ({ onClick }) => {
-  return <div className={styles.cross} onClick={onClick}></div>;
+  return <div className={styles.cross} onClick={onClick} />;
 };
 
 export default CloseButton;
