fab42d3d138620c084b1938f86f8f2836bfd90cf
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-25 11:23:09 -0500

    Built LogoBlockList

--- components/logoBlockList/logoBlockList.js
+++ components/logoBlockList/logoBlockList.js
@@ -1,5 +1,27 @@
-const LogoBlockList = () => {
-  return <></>;
+import clsx from "clsx";
+import LogoBlock from "../logoBlock/logoBlock";
+import styles from "./logoBlockList.module.scss";
+
+const LogoBlockList = ({ logoBlocks }) => {
+  if (!logoBlocks) {
+    return null;
+  } else {
+    return (
+      <ul className={styles.listContainer}>
+        {logoBlocks.map((block, index) => (
+          <li
+            className={clsx(
+              styles.blockContainer,
+              block.hasBackground ? styles.blockBackground : undefined
+            )}
+            key={index}
+          >
+            <LogoBlock block={block} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
 };
 
 export default LogoBlockList;
