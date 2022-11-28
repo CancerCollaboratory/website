ec9ffd592b30aa2942cefcafe15c618f690b34b0
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-28 17:05:08 -0500

    Built CollaboratorCard

--- components/collaboratorCard/collaboratorCard.js
+++ components/collaboratorCard/collaboratorCard.js
@@ -1,7 +1,21 @@
+import NavLine from "../../components/navLine/navLine";
 import styles from "./collaboratorCard.module.scss";
 
-const CollaboratorCard = () => {
-  return <></>;
+const CollaboratorCard = ({ title, description, buttonItems, children }) => {
+  if (!title || !description || !buttonItems || !children) {
+    return null;
+  } else {
+    return (
+      <div className={styles.container}>
+        {children}
+        <div className={styles.cardBody}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.description}>{description}</p>
+          <NavLine navItems={buttonItems} />
+        </div>
+      </div>
+    );
+  }
 };
 
 export default CollaboratorCard;
