a42126bf7afb7100c66133c23815ed8fb3f88cb8
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-28 14:26:59 -0500

    Edited LateralColumn

--- components/lateralColumn/lateralColumn.js
+++ components/lateralColumn/lateralColumn.js
@@ -3,13 +3,13 @@ import Column from "../column/column";
 import utilStyles from "../../styles/utils.module.scss";
 import styles from "./lateralColumn.module.scss";
 
-const LateralColumn = ({ leftColumn, rightColumn, styleBackground }) => {
+const LateralColumn = ({ leftColumn, rightColumn, styleBackground, styleColumnBorder }) => {
   return (
     <div
       className={clsx(styles.lateralColumnContainer, utilStyles.outerContainer, styleBackground)}
     >
-      {leftColumn && <Column styleColumn={styles.leftColumn}>{leftColumn}</Column>}
-      {rightColumn && <Column styleColumn={styles.rightColumn}>{rightColumn}</Column>}
+      {leftColumn && <Column styleColumn={styleColumnBorder}>{leftColumn}</Column>}
+      {rightColumn && <Column>{rightColumn}</Column>}
     </div>
   );
 };
