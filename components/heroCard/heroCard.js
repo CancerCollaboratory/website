4e7f85af435f47e9a24797f02720ce5f8eabdfe4
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-30 10:28:02 -0500

    Built HeroCard

---
+++ components/heroCard/heroCard.js
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import Image from "next/future/image";
+import Button from "../button/button";
+import styles from "./heroCard.module.scss";
+
+const HeroCard = ({ title, imageSrcPath, imageAlt, button, styleBackground, children }) => {
+  if (!title || !imageSrcPath || !button || !children) {
+    return null;
+  } else {
+    return (
+      <div className={styles.container}>
+        <Image
+          src={imageSrcPath}
+          alt={imageAlt}
+          className={styles.image}
+          quality={100}
+          width={76}
+          height={76}
+        />
+        <div className={clsx(styles.card, styleBackground)}>
+          <h2 className={styles.title}>{title}</h2>
+          {children}
+          <Button
+            label={button.label}
+            link={button.link}
+            isLinkExternal={button.isLinkExternal}
+            sideNote={button.sideNote}
+            hasIcon={button.hasIcon}
+          />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default HeroCard;
