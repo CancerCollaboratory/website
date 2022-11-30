d9037b2b5ab394facf1ac5771fefc5d11e62ffcf
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-30 10:32:25 -0500

    Edited Button

--- components/button/button.js
+++ components/button/button.js
@@ -4,30 +4,34 @@ import IconArrow from "../../public/assets/common/icon-arrow-white.svg";
 import styles from "./button.module.scss";
 
 const Button = ({ label, link, isLinkExternal, hasIcon }) => {
-  const buttonContent = (
-    <div className={styles.container}>
-      <p className={styles.label}>{label}</p>
-      {hasIcon && (
-        <Image
-          src={IconArrow}
-          alt={"Icon"}
-          link={link}
-          className={styles.iconArrow}
-          quality={100}
-          width={12}
-          height={12}
-        />
-      )}
-    </div>
-  );
+  if (!label || !link) {
+    return null;
+  } else {
+    const buttonContent = (
+      <div className={styles.container}>
+        <p className={styles.label}>{label}</p>
+        {hasIcon && (
+          <Image
+            src={IconArrow}
+            alt={"Icon"}
+            link={link}
+            className={styles.iconArrow}
+            quality={100}
+            width={12}
+            height={12}
+          />
+        )}
+      </div>
+    );
 
-  return isLinkExternal ? (
-    <a href={link} target="_blank" rel="noreferrer">
-      {buttonContent}
-    </a>
-  ) : (
-    <Link href={link}>{buttonContent}</Link>
-  );
+    return isLinkExternal ? (
+      <a href={link} target="_blank" rel="noreferrer">
+        {buttonContent}
+      </a>
+    ) : (
+      <Link href={link}>{buttonContent}</Link>
+    );
+  }
 };
 
 export default Button;
