2786f75bfdf256fe9bea55f26ec5b2b28845e15b
Dhyey Patel <97138744+dhyeypatell@users.noreply.github.com>
2022-11-29 22:37:19 -0500

    #49 - Create Collaborator Card Component
    * Built CollabCard and CollabCardLayout component
    
    * Added styling and responsiveness to them
    
    * Added data for CollabCard in about-funders-collaborators.json
    
    * Added some assets for CollabCard in assets/collabCard
    
    * Added a title prop to LateralColumn

--- components/column/column.js
+++ components/column/column.js
@@ -2,7 +2,11 @@ import clsx from "clsx";
 import styles from "./column.module.scss";
 
 const Column = ({ styleColumn, children }) => {
-  return <div className={clsx(styles.columnContainer, styleColumn)}>{children}</div>;
+  if (!children) {
+    return null;
+  } else {
+    return <div className={clsx(styles.container, styleColumn)}>{children}</div>;
+  }
 };
 
 export default Column;
