eb7c4b9755d140d3106e09173b5121c855fcbbfc
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-11-30 10:55:15 -0500

    Edited Button

--- components/button/button.js
+++ components/button/button.js
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import Image from "next/future/image";
+import CustomLink from "../customLink/customLink";
 import IconArrow from "../../public/assets/common/icon-arrow-white.svg";
 import styles from "./button.module.scss";
 
@@ -7,38 +7,27 @@ const Button = ({ tag, label, link, isLinkExternal, hasIcon }) => {
   if (!label || !link) {
     return null;
   } else {
-    const buttonContent = (
-      <div className={styles.innerContainer}>
-        <p className={styles.label}>{label}</p>
-        {hasIcon && (
-          <Image
-            src={IconArrow}
-            alt={"Icon"}
-            link={link}
-            className={styles.iconArrow}
-            quality={100}
-            width={12}
-            height={12}
-          />
-        )}
-      </div>
-    );
-
-    if (!isLinkExternal) {
+    return (
       <div className={styles.container}>
         {tag && <p className={styles.tag}>{tag}</p>}
-        <Link href={link}>{buttonContent}</Link>
-      </div>;
-    } else {
-      return (
-        <div className={styles.container}>
-          {tag && <p className={styles.tag}>{tag}</p>}
-          <a href={link} target="_blank" rel="noreferrer">
-            {buttonContent}
-          </a>
-        </div>
-      );
-    }
+        <CustomLink link={link} isLinkExternal={isLinkExternal}>
+          <div className={styles.innerContainer}>
+            <p className={styles.label}>{label}</p>
+            {hasIcon && (
+              <Image
+                src={IconArrow}
+                alt={"Icon"}
+                link={link}
+                className={styles.iconArrow}
+                quality={100}
+                width={12}
+                height={12}
+              />
+            )}
+          </div>
+        </CustomLink>
+      </div>
+    );
   }
 };
 
