52ef8debbd3560825b63a6965e4bfcfd6728573d
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-01 15:33:25 -0500

    Built FileLink component

---
+++ components/fileLink/fileLink.js
@@ -0,0 +1,35 @@
+import Image from "next/future/image";
+import CustomLink from "../customLink/customLink";
+import styles from "./fileLink.module.scss";
+
+const FileLink = ({ links }) => {
+  if (!links) {
+    return null;
+  } else {
+    return (
+      <ul className={styles.container}>
+        {links.map(
+          (item, index) =>
+            item.icon?.srcPath &&
+            item.icon?.alt &&
+            item.label &&
+            item.link && (
+              <li key={index}>
+                <Image
+                  src={item.icon.srcPath}
+                  alt={item.icon.alt}
+                  className={styles.icon}
+                  quality={100}
+                  width={16}
+                  height={16}
+                />
+                <CustomLink label={item.label} link={item.link} isLinkExternal={true} />
+              </li>
+            )
+        )}
+      </ul>
+    );
+  }
+};
+
+export default FileLink;
