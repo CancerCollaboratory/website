f328735d3dc747099d64eb3625a0d5fbb85289fb
Dhyey Patel <97138744+dhyeypatell@users.noreply.github.com>
2022-12-02 13:35:03 -0500

    #51 - Create File Link Component
    * Built FileLink component
    
    * Styled it and made it responsive
    
    * Added assets for FileLink in /public/assets/fileLink/
    
    * Added data for FileLink in /data/pages/privacy/privacy-policy.json

---
+++ components/fileLink/fileLink.js
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+import Image from "next/future/image";
+import CustomLink from "../customLink/customLink";
+import utilStyles from "../../styles/utils.module.scss";
+import styles from "./fileLink.module.scss";
+
+const FileLink = ({ links }) => {
+  if (!links) {
+    return null;
+  } else {
+    return (
+      <ul className={clsx(styles.container, utilStyles.outerContainer)}>
+        {links.map((item, index) => {
+          if (!item.icon?.srcPath || !item.icon?.alt || !item.label || !item.link) {
+            return null;
+          } else {
+            return (
+              <li key={index}>
+                <div className={styles.content}>
+                  <Image
+                    src={item.icon.srcPath}
+                    alt={item.icon.alt}
+                    className={styles.icon}
+                    quality={100}
+                    width={18}
+                    height={18}
+                  />
+                  <CustomLink label={item.label} link={item.link} isLinkExternal={true} />
+                </div>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    );
+  }
+};
+
+export default FileLink;
