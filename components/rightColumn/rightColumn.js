63c154e1498825395edb62ac5804c675fc91f38e
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-05 15:56:21 -0500

    Edited RightColumn

--- components/rightColumn/rightColumn.js
+++ components/rightColumn/rightColumn.js
@@ -6,7 +6,9 @@ import styles from "./rightColumn.module.scss";
 const RightColumn = ({ title, subTitle, body, children }) => {
   return (
     <div className={styles.container}>
-      {title && <h2 className={styles.title}>{title}</h2>}
+      {title && (
+        <h2 className={clsx(styles.title, !subTitle ? styles.titleMargin : undefined)}>{title}</h2>
+      )}
       {subTitle && <p className={styles.subTitle}>{subTitle}</p>}
       {body?.map(
         (item, index) =>
