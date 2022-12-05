ca8f0e86c6f4a28e01f0537394d876bd210424a1
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-05 13:55:10 -0500

    Used LateralColumn on AboutCollaboratory page

--- pages/about-collaboratory/index.js
+++ pages/about-collaboratory/index.js
@@ -1,10 +1,25 @@
 import Jumbotron from "../../components/jumbotron/jumbotron";
+import LateralColumn from "../../components/lateralColumn/lateralColumn";
+import LeftColumn from "../../components/leftColumn/leftColumn";
+import RightColumn from "../../components/rightColumn/rightColumn";
 import pageData from "../../data/pages/about/about-collaboratory.json";
+import utilStyles from "../../styles/utils.module.scss";
 
 const AboutCollaboratory = () => {
   const jumbotron = pageData.jumbotron;
-
-  return <Jumbotron title={jumbotron.title} bodyText={jumbotron.body.text} />;
+  const lateralColumn = pageData.lateralColumn;
+  return (
+    <>
+      <Jumbotron title={jumbotron.title} bodyText={jumbotron.body.text} />
+      <div className={utilStyles.pageBodyContainer}>
+        <LateralColumn
+          title={lateralColumn.title}
+          leftColumn={<LeftColumn body={lateralColumn.leftColumn.body} />}
+          rightColumn={<RightColumn body={lateralColumn.rightColumn.body} />}
+        />
+      </div>
+    </>
+  );
 };
 
 export default AboutCollaboratory;
