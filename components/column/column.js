b2e6c409f945360fae34ad4fe236c23daedb886c
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-05 22:43:20 -0500

    Edited Column

--- components/column/column.js
+++ components/column/column.js
@@ -1,11 +1,11 @@
 import clsx from "clsx";
 import styles from "./column.module.scss";
 
-const Column = ({ styleColumn, children }) => {
+const Column = ({ stylePadding, styleBorder, children }) => {
   if (!children) {
     return null;
   } else {
-    return <div className={clsx(styles.container, styleColumn)}>{children}</div>;
+    return <div className={clsx(styles.container, stylePadding, styleBorder)}>{children}</div>;
   }
 };
 
