4977282c2d12853512b1fd283918abcd3eb192c6
Dhyey Patel <97138744+dhyeypatell@users.noreply.github.com>
2022-12-08 18:54:39 -0500

    #83 - Finish "Privacy Policy" Page
    * Built PrivacyPolicy page

--- pages/privacy-policy/index.js
+++ pages/privacy-policy/index.js
@@ -1,10 +1,20 @@
 import Jumbotron from "../../components/jumbotron/jumbotron";
+import FileLink from "../../components/fileLink/fileLink";
 import pageData from "../../data/pages/privacy/privacy-policy.json";
+import utilStyles from "../../styles/utils.module.scss";
 
 const PrivacyPolicy = () => {
   const jumbotron = pageData.jumbotron;
+  const fileLink = pageData.fileLink;
 
-  return <Jumbotron title={jumbotron.title} />;
+  return (
+    <>
+      <Jumbotron title={jumbotron?.title} />
+      <div className={utilStyles.pageBodyContainer}>
+        <FileLink links={fileLink?.links} />
+      </div>
+    </>
+  );
 };
 
 export default PrivacyPolicy;
