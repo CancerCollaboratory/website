6dc81d348535af43060a5774fc72e7f32cd7a429
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-08 18:24:14 -0500

    Edited CommonHead

--- components/commonHead/commonHead.js
+++ components/commonHead/commonHead.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 const CommonHead = () => {
   return (
     <Head>
+      <title>Cancer Genome Collaboratory</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
