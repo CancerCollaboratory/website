9dcf573264fb97fabb230d37ef77980a78144882
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-08 17:00:41 -0500

    Used Favicon in MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import BannerLayout from "../components/bannerLayout/bannerLayout";
+import Favicon from "../components/favicon/favicon";
 import Layout from "../components/layout/layout";
 import "../styles/reset.scss";
 import "../styles/default.scss";
@@ -7,10 +8,16 @@ export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
   if (Component.getLayout) {
-    return <BannerLayout>{getLayout(<Component {...pageProps} />)}</BannerLayout>;
+    return (
+      <BannerLayout>
+        <Favicon />
+        {getLayout(<Component {...pageProps} />)}
+      </BannerLayout>
+    );
   } else {
     return (
       <BannerLayout>
+        <Favicon />
         <Layout>
           <Component {...pageProps} />
         </Layout>
