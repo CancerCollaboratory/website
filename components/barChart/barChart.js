3a8948a20c6a388de7a56dc21566d019c5b1aa3e
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-09 15:04:35 -0500

    Built BarChart

--- components/barChart/barChart.js
+++ components/barChart/barChart.js
@@ -1,5 +1,18 @@
-const BarChart = () => {
-  return <></>;
+import Image from "next/image";
+import styles from "./barChart.module.scss";
+
+const BarChart = ({ srcPath, alt }) => {
+  return (
+    <Image
+      src={srcPath}
+      alt={alt}
+      priority={true}
+      className={styles.chart}
+      quality={100}
+      width={550}
+      height={550}
+    />
+  );
 };
 
 export default BarChart;
