22f661bff8d04892b32ee6b46805ccc9c04b8021
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-13 18:08:22 -0500

    Edited FeatureBoard

--- components/featureBoard/featureBoard.js
+++ components/featureBoard/featureBoard.js
@@ -1,10 +1,11 @@
 import clsx from "clsx";
+import CustomLink from "../customLink/customLink";
 import styles from "./featureBoard.module.scss";
 
 const FeatureBoard = ({ boards }) => {
   return (
     <div className={styles.container}>
-      {boards.map((board, index) => {
+      {boards?.map((board, index) => {
         if (!board.title || !board.caption) {
           return null;
         } else {
@@ -12,11 +13,20 @@ const FeatureBoard = ({ boards }) => {
             <div
               className={clsx(
                 styles.innerContainer,
-                board.hasBackground ? styles.background : undefined
+                board.hasBackground ? styles.background : undefined,
+                index % 2 !== 0 ? styles.addBackground : undefined
               )}
               key={index}
             >
-              <p className={styles.title}>{board.title}</p>
+              {!board.titleLink && <p className={styles.title}>{board.title}</p>}
+              {board.titleLink && (
+                <CustomLink
+                  label={board.title}
+                  link={board.titleLink}
+                  isLinkExternal={board.isTitleLinkExternal}
+                  styleLabel={styles.titleLink}
+                />
+              )}
               <p className={styles.caption}>{board.caption}</p>
             </div>
           );
