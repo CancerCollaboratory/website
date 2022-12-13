956dceb118fc6287def5aa3d8083f6fecdc08638
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-13 17:45:20 -0500

    Built FeatureBoard

--- components/featureBoard/featureBoard.js
+++ components/featureBoard/featureBoard.js
@@ -1,7 +1,29 @@
+import clsx from "clsx";
 import styles from "./featureBoard.module.scss";
 
-const FeatureBoard = () => {
-  return <div className={styles.container}></div>;
+const FeatureBoard = ({ boards }) => {
+  return (
+    <div className={styles.container}>
+      {boards.map((board, index) => {
+        if (!board.title || !board.caption) {
+          return null;
+        } else {
+          return (
+            <div
+              className={clsx(
+                styles.innerContainer,
+                board.hasBackground ? styles.background : undefined
+              )}
+              key={index}
+            >
+              <p className={styles.title}>{board.title}</p>
+              <p className={styles.caption}>{board.caption}</p>
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
 };
 
 export default FeatureBoard;
