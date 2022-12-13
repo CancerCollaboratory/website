c07cdfa47912efd022ccabe729b31158e16b02c6
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-13 09:25:20 -0500

    Edited ResourceCardLayout

--- components/resourceCardLayout/resourceCardLayout.js
+++ components/resourceCardLayout/resourceCardLayout.js
@@ -1,20 +1,38 @@
 import ResourceCard from "../resourceCard/resourceCard";
+import ListLine from "../listLine/listLine";
 import styles from "./resourceCardLayout.module.scss";
 
 const ResourceCardLayout = ({ cards }) => {
   return (
     <div className={styles.container}>
-      {cards.map((card, index) => (
-        <ResourceCard
-          title={card.title}
-          isTitleLinkExternal={card.isTitleLinkExternal}
-          bodyText={card.bodyText}
-          imageSrcPath={card.imageSrcPath}
-          imageAlt={card.imageAlt}
-          isLastCard={card.isLastCard}
-          key={index}
-        />
-      ))}
+      {cards.map((card, index) => {
+        if (!card.listLine) {
+          return (
+            <ResourceCard
+              title={card.title}
+              isTitleLinkExternal={card.isTitleLinkExternal}
+              bodyText={card.bodyText}
+              imageSrcPath={card.imageSrcPath}
+              imageAlt={card.imageAlt}
+              isLastCard={card.isLastCard}
+              key={index}
+            />
+          );
+        } else {
+          return (
+            <ResourceCard
+              title={card.title}
+              isTitleLinkExternal={card.isTitleLinkExternal}
+              imageSrcPath={card.imageSrcPath}
+              imageAlt={card.imageAlt}
+              isLastCard={card.isLastCard}
+              key={index}
+            >
+              <ListLine isVertical={card.listLine.isVertical} items={card.listLine.items} />
+            </ResourceCard>
+          );
+        }
+      })}
     </div>
   );
 };
