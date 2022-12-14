9b27de1a87b4bdc5ed519d484fe5051a324fa22c
Dhyey Patel <97138744+dhyeypatell@users.noreply.github.com>
2022-12-14 14:11:37 -0500

    #97 - Build FeatureBoard Component
    * Built FeatureBoard Component
    
    * Styled it and made it responsive
    
    * Added an asset for Dockstore in public/assets/leftColumn
    
    * Added data for FeatureBoard in data/pages/services/services-dockstore.json

---
+++ components/featureBoard/featureBoard.js
@@ -0,0 +1,41 @@
+import clsx from "clsx";
+import CustomLink from "../customLink/customLink";
+import utilStyles from "../../styles/utils.module.scss";
+import styles from "./featureBoard.module.scss";
+
+const FeatureBoard = ({ boards }) => {
+  return (
+    <div className={styles.container}>
+      {boards?.map((board, index) => {
+        if (!board.title || !board.caption) {
+          return null;
+        } else {
+          return (
+            <div
+              className={clsx(
+                styles.innerContainer,
+                board.hasBackground ? styles.background : undefined,
+                index % 2 !== 0 ? styles.addBackground : undefined,
+                board.titleLink ? utilStyles.commonAnchor : undefined
+              )}
+              key={index}
+            >
+              {!board.titleLink && <p className={styles.title}>{board.title}</p>}
+              {board.titleLink && (
+                <CustomLink
+                  label={board.title}
+                  link={board.titleLink}
+                  isLinkExternal={board.isTitleLinkExternal}
+                  styleLabel={styles.titleLink}
+                />
+              )}
+              <p className={styles.caption}>{board.caption}</p>
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+export default FeatureBoard;
