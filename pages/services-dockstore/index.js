43a156f174af729547c08b3355896ac7f42a75bb
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-14 15:35:26 -0500

    Added content in Dockstore

--- pages/services-dockstore/index.js
+++ pages/services-dockstore/index.js
@@ -1,19 +1,53 @@
+import clsx from "clsx";
 import Jumbotron from "../../components/jumbotron/jumbotron";
+import LateralColumn from "../../components/lateralColumn/lateralColumn";
+import LeftColumn from "../../components/leftColumn/leftColumn";
+import RightColumn from "../../components/rightColumn/rightColumn";
+import FeatureBoard from "../../components/featureBoard/featureBoard";
 import pageData from "../../data/pages/services/services-dockstore.json";
+import utilStyles from "../../styles/utils.module.scss";
 
 const Dockstore = () => {
   const jumbotron = pageData.jumbotron;
+  const lateralColumn = pageData.lateralColumn;
 
   return (
-    <Jumbotron
-      title={jumbotron.title}
-      bodyText={jumbotron.body.text}
-      imageSrcPath={jumbotron.image.srcPath}
-      imageLabel={jumbotron.image.label}
-      buttonLabel={jumbotron.button.label}
-      isButtonLinkExternal={jumbotron.button.isLinkExternal}
-      buttonLink={jumbotron.button.link}
-    />
+    <>
+      <Jumbotron
+        title={jumbotron?.title}
+        bodyText={jumbotron?.body?.text}
+        imageSrcPath={jumbotron?.image?.srcPath}
+        imageLabel={jumbotron?.image?.label}
+        buttonLabel={jumbotron?.button?.label}
+        isButtonLinkExternal={jumbotron?.button?.isLinkExternal}
+        buttonLink={jumbotron?.button?.link}
+      />
+      <div className={utilStyles.pageBodyContainer}>
+        <LateralColumn
+          leftColumn={
+            <LeftColumn
+              title={lateralColumn?.leftColumn?.title}
+              imageSrcPath={lateralColumn?.leftColumn?.imageSrcPath}
+              imageAlt={lateralColumn?.leftColumn?.imageAlt}
+              body={lateralColumn?.leftColumn?.body}
+            />
+          }
+          rightColumn={
+            <RightColumn
+              title={lateralColumn?.rightColumn?.title}
+              body={lateralColumn?.rightColumn?.body}
+            >
+              <div
+                className={clsx(utilStyles.onlyChildContainer, utilStyles.horizontallyCenterItem)}
+              >
+                <FeatureBoard boards={lateralColumn?.rightColumn?.featureBoard?.boards} />
+              </div>
+            </RightColumn>
+          }
+          hasBorderBottom={lateralColumn?.hasBorderBottom}
+        />
+      </div>
+    </>
   );
 };
 
