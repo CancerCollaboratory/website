56ea52eebda426ac85e1246f5d1aded2dc929e32
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-15 14:45:04 -0500

    Edited BarChart

--- components/barChart/barChart.js
+++ components/barChart/barChart.js
@@ -2,21 +2,25 @@ import Image from "next/image";
 import styles from "./barChart.module.scss";
 
 const BarChart = ({ title, subTitle, srcPath, alt }) => {
-  return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.subTitle}>{subTitle}</p>
-      <Image
-        src={srcPath}
-        alt={alt}
-        priority={true}
-        className={styles.chart}
-        quality={100}
-        width={550}
-        height={550}
-      />
-    </div>
-  );
+  if (!title || !subTitle || !srcPath || !alt) {
+    return null;
+  } else {
+    return (
+      <div className={styles.container}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.subTitle}>{subTitle}</p>
+        <Image
+          src={srcPath}
+          alt={alt}
+          priority={true}
+          className={styles.chart}
+          quality={100}
+          width={550}
+          height={550}
+        />
+      </div>
+    );
+  }
 };
 
 export default BarChart;
