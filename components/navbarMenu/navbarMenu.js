ee24a84e86a6e836ec3a9a49c058394b22b4af72
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-15 08:59:58 -0500

    Edited NavbarMenu

--- components/navbarMenu/navbarMenu.js
+++ components/navbarMenu/navbarMenu.js
@@ -3,40 +3,21 @@ import CustomLink from "../customLink/customLink";
 import { useRouter } from "next/router";
 import styles from "../navbar/navbar.module.scss";
 
-const NavbarMenu = ({ styleMenu, styleSubMenu, menuItems, hasSubMenuDivison }) => {
+const NavbarMenu = ({ styleMenu, menuItems, showActivePage }) => {
   const { asPath } = useRouter();
 
   return (
     <div className={styleMenu}>
       {menuItems.map((item, index) => (
-        <React.Fragment key={index}>
-          <CustomLink
-            label={item.label}
-            link={item.link}
-            styleLabel={
-              hasSubMenuDivison &&
-              (item.link === asPath || asPath + "#" === item.link.slice(0, asPath.length + 1))
-                ? styles.activePage
-                : undefined
-            }
-          />
-          <ul className={styleSubMenu}>
-            {item.subMenuSection?.map((subItem, subIndex) => (
-              <li key={subIndex}>
-                <CustomLink
-                  label={subItem.label}
-                  link={subItem.link}
-                  styleLabel={
-                    hasSubMenuDivison && subItem.link === asPath ? styles.activePage : undefined
-                  }
-                />
-              </li>
-            ))}
-          </ul>
-          {hasSubMenuDivison && item.subMenuSection && (
-            <div className={styles.subMenuDivision}></div>
-          )}
-        </React.Fragment>
+        <ul key={index}>
+          <li>
+            <CustomLink
+              label={item.label}
+              link={item.link}
+              styleLabel={showActivePage && item.link === asPath ? styles.activePage : undefined}
+            />
+          </li>
+        </ul>
       ))}
     </div>
   );
