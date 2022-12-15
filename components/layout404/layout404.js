f3b1af4b3c0695df0ff14a4ee236c697a553277d
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-15 16:41:39 -0500

    Deleted unneeded files

--- components/layout404/layout404.js
+++
@@ -1,7 +0,0 @@
-import styles from "./layout404.module.scss";
-
-const Layout404 = ({ children }) => {
-  return <div className={styles.container404}>{children}</div>;
-};
-
-export default Layout404;
