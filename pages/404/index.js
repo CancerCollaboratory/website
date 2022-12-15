e3b7e34f8e4f4450002a7d756f45032e1e7d30fc
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-15 16:52:49 -0500

    Edited Custom404

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,18 +1,15 @@
-import Layout404 from "../../components/layout404/layout404";
 import pageData from "../../data/pages/404/404.json";
-import styles from "./404.module.scss";
+import utilStyles from "../../styles/utils.module.scss";
 
 const Custom404 = () => {
+  const custom404 = pageData.custom404;
+
   return (
-    <>
-      <h1 className={styles.errorTextLarge}>{pageData.error404.title}</h1>
-      <h2 className={styles.errorTextMedium}>{pageData.error404.message}</h2>
-    </>
+    <div className={utilStyles.pageBodyContainer}>
+      <h1>{custom404?.title}</h1>
+      <h2>{custom404?.subTitle}</h2>
+    </div>
   );
 };
 
 export default Custom404;
-
-Custom404.getLayout = function getLayout(page) {
-  return <Layout404>{page}</Layout404>;
-};
