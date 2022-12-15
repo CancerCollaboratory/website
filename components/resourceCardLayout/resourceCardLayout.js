d95efc5082659d0ad2d890b9f478b44513cb5997
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-15 18:20:16 -0500

    Edited ResourceCardLayout

--- components/resourceCardLayout/resourceCardLayout.js
+++ components/resourceCardLayout/resourceCardLayout.js
@@ -3,38 +3,42 @@ import ListLine from "../listLine/listLine";
 import styles from "./resourceCardLayout.module.scss";
 
 const ResourceCardLayout = ({ cards }) => {
-  return (
-    <div className={styles.container}>
-      {cards.map((card, index) => {
-        if (!card.listLine) {
-          return (
-            <ResourceCard
-              title={card.title}
-              isTitleLinkExternal={card.isTitleLinkExternal}
-              bodyText={card.bodyText}
-              imageSrcPath={card.imageSrcPath}
-              imageAlt={card.imageAlt}
-              isLastCard={card.isLastCard}
-              key={index}
-            />
-          );
-        } else {
-          return (
-            <ResourceCard
-              title={card.title}
-              isTitleLinkExternal={card.isTitleLinkExternal}
-              imageSrcPath={card.imageSrcPath}
-              imageAlt={card.imageAlt}
-              isLastCard={card.isLastCard}
-              key={index}
-            >
-              <ListLine isVertical={card.listLine?.isVertical} items={card.listLine?.items} />
-            </ResourceCard>
-          );
-        }
-      })}
-    </div>
-  );
+  if (!cards) {
+    return null;
+  } else {
+    return (
+      <div className={styles.container}>
+        {cards.map((card, index) => {
+          if (!card.listLine) {
+            return (
+              <ResourceCard
+                title={card.title}
+                isTitleLinkExternal={card.isTitleLinkExternal}
+                bodyText={card.bodyText}
+                imageSrcPath={card.imageSrcPath}
+                imageAlt={card.imageAlt}
+                isLastCard={card.isLastCard}
+                key={index}
+              />
+            );
+          } else {
+            return (
+              <ResourceCard
+                title={card.title}
+                isTitleLinkExternal={card.isTitleLinkExternal}
+                imageSrcPath={card.imageSrcPath}
+                imageAlt={card.imageAlt}
+                isLastCard={card.isLastCard}
+                key={index}
+              >
+                <ListLine isVertical={card.listLine.isVertical} items={card.listLine.items} />
+              </ResourceCard>
+            );
+          }
+        })}
+      </div>
+    );
+  }
 };
 
 export default ResourceCardLayout;
