7cd19c97e9c8a98964c953edccdef03d2f0d3d25
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-16 11:48:07 -0500

    Edited CloudResources

--- pages/services-cloud-resources/index.js
+++ pages/services-cloud-resources/index.js
@@ -1,15 +1,32 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Jumbotron from "../../components/jumbotron/jumbotron";
 import pageData from "../../data/pages/services/services-cloud-resources.json";
+import utilStyles from "../../styles/utils.module.scss";
 
 const CloudResources = () => {
+  const { asPath } = useRouter();
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
   const jumbotron = pageData.jumbotron;
+  const tabLayout = pageData.tabLayout;
+
+  function getActiveTabIndex(currentPath) {
+    return tabLayout?.tabs?.findIndex((tab) => tab.link === currentPath);
+  }
+
+  useEffect(() => {
+    setActiveTabIndex(getActiveTabIndex(asPath) === -1 ? 0 : getActiveTabIndex(asPath));
+  }, [asPath]);
 
   return (
-    <Jumbotron
-      title={jumbotron?.title}
-      bodyText={jumbotron?.body?.text}
-      isBodyLinkExternal={jumbotron?.body?.isLinkExternal}
-    />
+    <>
+      <Jumbotron
+        title={jumbotron?.title}
+        bodyText={jumbotron?.body?.text}
+        isBodyLinkExternal={jumbotron?.body?.isLinkExternal}
+      />
+      <div className={utilStyles.pageBodyContainer}></div>
+    </>
   );
 };
 
