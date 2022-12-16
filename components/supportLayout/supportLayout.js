da4c1435f66d60e59ca21f5c20e18e8c8ac07eba
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-15 19:47:19 -0500

    Edited SupportLayout

--- components/supportLayout/supportLayout.js
+++ components/supportLayout/supportLayout.js
@@ -1,16 +1,23 @@
 import Navbar from "../navbar/navbar";
 import SupportSidebar from "../supportSidebar/supportSidebar";
 import Footer from "../footer/footer";
+import styles from "./supportLayout.module.scss";
 
 const SupportLayout = ({ children }) => {
-  return (
-    <>
-      <Navbar />
-      <SupportSidebar />
-      {children}
-      <Footer />
-    </>
-  );
+  if (!children) {
+    return null;
+  } else {
+    return (
+      <>
+        <Navbar />
+        <div className={styles.container}>
+          <SupportSidebar />
+          {children}
+        </div>
+        <Footer />
+      </>
+    );
+  }
 };
 
 export default SupportLayout;
