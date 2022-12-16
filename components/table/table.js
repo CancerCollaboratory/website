662570f5c0ca0dba7cdcae9653040a6a83825aa6
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-15 19:10:20 -0500

    Edited Table

--- components/table/table.js
+++ components/table/table.js
@@ -5,27 +5,31 @@ import utilStyles from "../../styles/utils.module.scss";
 import styles from "./table.module.scss";
 
 const Table = ({ title, body }) => {
-  return (
-    <table className={styles.table}>
-      {title && <caption className={styles.title}>{title}</caption>}
-      <tbody>
-        {body?.rows?.map((item, index) => (
-          <tr key={index}>
-            {item.cells?.map((cell, subIndex) => (
-              <td className={clsx(styles.cell, utilStyles.commonAnchor)} key={subIndex}>
-                {cell.title &&
-                  renderData(
-                    cell.title,
-                    cell.isLinkExternal ? insertLinkExternal : insertLinkInternal
-                  )}
-                {cell.stat && <div className={styles.cellContent}>{renderData(cell.stat)}</div>}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+  if (!title || !body) {
+    return null;
+  } else {
+    return (
+      <table className={styles.table}>
+        <caption className={styles.title}>{title}</caption>
+        <tbody>
+          {body.rows?.map((item, index) => (
+            <tr key={index}>
+              {item.cells?.map((cell, subIndex) => (
+                <td className={clsx(styles.cell, utilStyles.commonAnchor)} key={subIndex}>
+                  {cell.title &&
+                    renderData(
+                      cell.title,
+                      cell.isLinkExternal ? insertLinkExternal : insertLinkInternal
+                    )}
+                  {cell.stat && <div className={styles.cellContent}>{renderData(cell.stat)}</div>}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 };
 
 export default Table;
