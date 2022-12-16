5cd2fa8a153256568b487e6da79e472f5c53f8ce
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-15 19:28:50 -0500

    Edited BannerLayout

--- components/bannerLayout/bannerLayout.js
+++ components/bannerLayout/bannerLayout.js
@@ -5,27 +5,31 @@ import bannerData from "../../data/components/banner/banner.json";
 const BannerLayout = ({ children }) => {
   const banner = bannerData.banner;
 
-  if (banner?.isActive !== true) {
-    return children;
+  if (!children) {
+    return null;
   } else {
-    return (
-      <>
-        <Banner
-          title={banner?.title}
-          description={banner?.description}
-          isDescriptionLinkExternal={banner?.isDescriptionLinkExternal}
-        >
-          {banner?.linkLabel && banner?.linkTarget && (
-            <CustomLink
-              label={banner?.linkLabel}
-              link={banner?.linkTarget}
-              isLinkExternal={banner?.isLinkTargetExternal}
-            />
-          )}
-        </Banner>
-        {children}
-      </>
-    );
+    if (banner?.isActive !== true) {
+      return children;
+    } else {
+      return (
+        <>
+          <Banner
+            title={banner?.title}
+            description={banner?.description}
+            isDescriptionLinkExternal={banner?.isDescriptionLinkExternal}
+          >
+            {banner?.linkLabel && banner?.linkTarget && (
+              <CustomLink
+                label={banner.linkLabel}
+                link={banner.linkTarget}
+                isLinkExternal={banner.isLinkTargetExternal}
+              />
+            )}
+          </Banner>
+          {children}
+        </>
+      );
+    }
   }
 };
 
