4a55d780cad9a36b39e369133dc1f8a67ba7d1fa
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-17 20:02:26 -0500

    Created PageNotFoundMessage

---
+++ components/pageNotFoundMessage/pageNotFoundMessage.js
@@ -0,0 +1,7 @@
+import styles from "./pageNotFoundMessage.module.scss";
+
+const PageNotFoundMessage = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default PageNotFoundMessage;
