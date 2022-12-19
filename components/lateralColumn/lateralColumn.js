570e551a9c8f56526180a217f0f1b41f22f1ffa7
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-18 21:18:09 -0500

    Edited LateralColumn

--- components/lateralColumn/lateralColumn.js
+++ components/lateralColumn/lateralColumn.js
@@ -44,9 +44,7 @@ const LateralColumn = ({
                 height={62}
               />
             )}
-            {title && (
-              <h2 className={clsx(styles.title, utilStyles.defaultElementLineHeight)}>{title}</h2>
-            )}
+            {title && <h2 className={utilStyles.defaultElementLineHeight}>{title}</h2>}
           </div>
         )}
         <div className={clsx(styles.innerContainer, styleColumnAlignment)}>
