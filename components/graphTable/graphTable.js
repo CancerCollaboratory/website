c710290f3671a5c9cb08264cb976948cb135522e
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-19 13:54:00 -0500

    Added GraphTable

---
+++ components/graphTable/graphTable.js
@@ -0,0 +1,42 @@
+import clsx from "clsx";
+import { renderData } from "../../lib/functions/renderData";
+import { insertLinkExternal, insertLinkInternal } from "../../lib/functions/insertLink";
+import utilStyles from "../../styles/utils.module.scss";
+import styles from "./graphTable.module.scss";
+
+const GraphTable = ({ title, body }) => {
+  if (!title || !body) {
+    return null;
+  } else {
+    return (
+      <table className={styles.table}>
+        <caption className={styles.title}>{title}</caption>
+        <tbody>
+          {body.rows?.map((item, index) => (
+            <tr key={index}>
+              {item.cells?.map((cell, subIndex) => (
+                <td
+                  className={clsx(
+                    styles.cell,
+                    utilStyles.embeddedParagraphLineHeight,
+                    cell.isLinkExternal ? utilStyles.commonAnchor : undefined
+                  )}
+                  key={subIndex}
+                >
+                  {cell.title &&
+                    renderData(
+                      cell.title,
+                      cell.isLinkExternal ? insertLinkExternal : insertLinkInternal
+                    )}
+                  {cell.stat && <div className={styles.cellContent}>{renderData(cell.stat)}</div>}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+};
+
+export default GraphTable;
