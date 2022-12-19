b001082c09ab73221d512bedd7402f8ab0c4f9ad
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-19 08:49:07 -0500

    Edited Jumbotron

--- components/jumbotron/jumbotron.js
+++ components/jumbotron/jumbotron.js
@@ -14,7 +14,13 @@ const Jumbotron = ({ title, bodyText, isBodyLinkExternal, imageSrcPath, imageAlt
       <div className={clsx(styles.container, !bodyText ? styles.stretchBackground : undefined)}>
         <h1 className={styles.title}>{title}</h1>
         {bodyText && (
-          <div className={clsx(styles.bodyBlock, utilStyles.commonAnchor)}>
+          <div
+            className={clsx(
+              styles.bodyBlock,
+              utilStyles.commonAnchor,
+              utilStyles.embeddedParagraphLineHeight
+            )}
+          >
             {renderData(bodyText, isBodyLinkExternal ? insertLinkExternal : insertLinkInternal)}
           </div>
         )}
