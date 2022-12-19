07570972a256d60d9c5aedde735661a1f9a81233
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-19 15:12:28 -0500

    Edited CloudResources

--- pages/services-cloud-resources/index.js
+++ pages/services-cloud-resources/index.js
@@ -1,10 +1,11 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Jumbotron from "../../components/jumbotron/jumbotron";
+import TabSection from "../../components/tabSection/tabSection";
 import LateralColumn from "../../components/lateralColumn/lateralColumn";
 import LeftColumn from "../../components/leftColumn/leftColumn";
 import RightColumn from "../../components/rightColumn/rightColumn";
-import TabSection from "../../components/tabSection/tabSection";
+import BasicTable from "../../components/basicTable/basicTable";
 import pageData from "../../data/pages/services/services-cloud-resources.json";
 import utilStyles from "../../styles/utils.module.scss";
 
@@ -13,6 +14,7 @@ const CloudResources = () => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const jumbotron = pageData.jumbotron;
   const tabSection = pageData.tabSection;
+  const tabContent = tabSection?.tabLayout?.tabs[activeTabIndex]?.content;
 
   function getActiveTabIndex(pagePath) {
     return tabSection?.tabLayout?.tabs?.findIndex((tab) => tab.link === pagePath);
@@ -32,9 +34,15 @@ const CloudResources = () => {
       <div className={utilStyles.pageBodyContainer}>
         <TabSection>
           <LateralColumn
-            title={tabSection?.tabLayout?.tabs[activeTabIndex]?.content?.lateralColumn?.title}
-            leftColumn={<LeftColumn />}
-            rightColumn={<RightColumn />}
+            leftColumn={<LeftColumn body={tabContent?.lateralColumn?.leftColumn?.body} />}
+            rightColumn={
+              <RightColumn body={tabContent?.lateralColumn?.rightColumn?.body}>
+                <BasicTable
+                  headRow={tabContent?.lateralColumn?.rightColumn?.basicTable?.headRow}
+                  body={tabContent?.lateralColumn?.rightColumn?.basicTable?.body}
+                />
+              </RightColumn>
+            }
           />
         </TabSection>
       </div>
