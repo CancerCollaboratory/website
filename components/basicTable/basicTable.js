86e746d6700570bbf399dd190b4e886e39647f75
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-19 14:15:56 -0500

    Built BasicTable

--- components/basicTable/basicTable.js
+++ components/basicTable/basicTable.js
@@ -1,5 +1,26 @@
-const BasicTable = () => {
-  return <table></table>;
+import styles from "./basicTable.module.scss";
+
+const BasicTable = ({ tableHeadRow, tableBody }) => {
+  if (!tableHeadRow || !tableBody) {
+    return null;
+  } else {
+    return (
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {tableHeadRow.map((cell, index) => cell.value && <th key={index}>{cell.value}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {tableBody.map((row, index) => (
+            <tr className={row.hasBackground ? styles.rowBackground : undefined} key={index}>
+              {row.map((cell, subIndex) => cell.value && <td key={subIndex}>{cell.value}</td>)}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 };
 
 export default BasicTable;
