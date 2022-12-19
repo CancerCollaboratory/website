b904ff0f051c28871ebb52a36a9f00b0ffef3a5f
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-19 11:07:51 -0500

    Edited PageNotFoundMessage

--- components/pageNotFoundMessage/pageNotFoundMessage.js
+++ components/pageNotFoundMessage/pageNotFoundMessage.js
@@ -2,30 +2,19 @@ import clsx from "clsx";
 import React from "react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import data from "../../data/pages/404/404.json";
 import utilStyles from "../../styles/utils.module.scss";
 import styles from "./pageNotFoundMessage.module.scss";
 
 const PageNotFoundMessage = () => {
   const { asPath } = useRouter();
   const [pagePath, setPagePath] = useState("");
-  const errorMessage = data.pageNotFoundMessage;
 
   useEffect(() => setPagePath(asPath), []);
-
-  if (!errorMessage?.part1 || !errorMessage?.part2) {
-    return null;
-  } else {
-    return (
-      <div className={clsx(styles.container, utilStyles.outerContainer)}>
-        <p>
-          <span>{errorMessage.part1}</span>
-          {errorMessage?.insertPagePath && <span>{` "${pagePath}" `}</span>}
-          <span>{errorMessage.part2}</span>
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div className={clsx(styles.container, utilStyles.outerContainer)}>
+      <p>{`The requested page "${pagePath}" could not be found.`}</p>
+    </div>
+  );
 };
 
 export default PageNotFoundMessage;
