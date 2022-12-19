17e7b2f5d7f80dc42f19b29abc50c2ba9bc7aab1
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-19 14:32:59 -0500

    Edited BasicTable

--- components/basicTable/basicTable.js
+++ components/basicTable/basicTable.js
@@ -1,18 +1,16 @@
 import styles from "./basicTable.module.scss";
 
-const BasicTable = ({ tableHeadRow, tableBody }) => {
-  if (!tableHeadRow || !tableBody) {
+const BasicTable = ({ headRow, body }) => {
+  if (!headRow || !body) {
     return null;
   } else {
     return (
       <table className={styles.table}>
         <thead>
-          <tr>
-            {tableHeadRow.map((cell, index) => cell.value && <th key={index}>{cell.value}</th>)}
-          </tr>
+          <tr>{headRow.map((cell, index) => cell.value && <th key={index}>{cell.value}</th>)}</tr>
         </thead>
         <tbody>
-          {tableBody.map((row, index) => (
+          {body.map((row, index) => (
             <tr className={row.hasBackground ? styles.rowBackground : undefined} key={index}>
               {row.map((cell, subIndex) => cell.value && <td key={subIndex}>{cell.value}</td>)}
             </tr>
