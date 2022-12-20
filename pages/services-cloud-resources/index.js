017635812ac3e9d00780c18302c975d943b1789d
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-20 07:45:38 -0500

    Edited CloudResources

--- pages/services-cloud-resources/index.js
+++ pages/services-cloud-resources/index.js
@@ -1,12 +1,18 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Jumbotron from "../../components/jumbotron/jumbotron";
+import LateralColumn from "../../components/lateralColumn/lateralColumn";
+import LeftColumn from "../../components/leftColumn/leftColumn";
+import RightColumn from "../../components/rightColumn/rightColumn";
+import LogoBlockList from "../../components/logoBlockList/logoBlockList";
 import pageData from "../../data/pages/services/services-cloud-resources.json";
+import utilStyles from "../../styles/utils.module.scss";
 
 const CloudResources = () => {
   const { asPath } = useRouter();
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const jumbotron = pageData.jumbotron;
+  const lateralColumn1 = pageData.lateralColumn1;
   const tabSection = pageData.tabSection;
   const tabContent = tabSection?.tabLayout?.tabs[activeTabIndex]?.content;
 
@@ -19,11 +25,35 @@ const CloudResources = () => {
   }, [asPath]);
 
   return (
-    <Jumbotron
-      title={jumbotron?.title}
-      bodyText={jumbotron?.body?.text}
-      isBodyLinkExternal={jumbotron?.body?.isLinkExternal}
-    />
+    <>
+      <Jumbotron
+        title={jumbotron?.title}
+        bodyText={jumbotron?.body?.text}
+        isBodyLinkExternal={jumbotron?.body?.isLinkExternal}
+      />
+      <div className={utilStyles.pageBodyContainer}>
+        <LateralColumn
+          leftColumn={
+            <LeftColumn
+              title={lateralColumn1?.leftColumn?.title}
+              imageSrcPath={lateralColumn1?.leftColumn?.imageSrcPath}
+              imageAlt={lateralColumn1?.leftColumn?.imageAlt}
+              body={lateralColumn1?.leftColumn?.body}
+            />
+          }
+          rightColumn={
+            <RightColumn>
+              <div className={utilStyles.peakWidth}>
+                <LogoBlockList
+                  logoBlocks={lateralColumn1?.rightColumn?.logoBlockList?.logoBlocks}
+                />
+              </div>
+            </RightColumn>
+          }
+          styleColumnAlignment={utilStyles.verticallyCenterItem}
+        />
+      </div>
+    </>
   );
 };
 
