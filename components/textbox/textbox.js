d16474401290ef7823a552616f8e109467b07b03
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-19 19:42:56 -0500

    Built Textbox

--- components/textbox/textbox.js
+++ components/textbox/textbox.js
@@ -1,5 +1,50 @@
-const Textbox = () => {
-  return <></>;
+import clsx from "clsx";
+import Image from "next/image";
+import { renderData } from "../../lib/functions/renderData";
+import { insertLinkExternal, insertLinkInternal } from "../../lib/functions/insertLink";
+import utilStyles from "../../styles/utils.module.scss";
+import styles from "./textbox.module.scss";
+
+const Textbox = ({ title, titleImageSrcPath, titleImageAlt, body, children }) => {
+  return (
+    <div className={styles.container}>
+      {(title || (titleImageSrcPath && titleImageAlt)) && (
+        <div className={clsx(styles.head, utilStyles.defaultElementMarginBottom)}>
+          {titleImageSrcPath && titleImageAlt && (
+            <Image
+              src={titleImageSrcPath}
+              alt={titleImageAlt}
+              className={utilStyles.smallTitleIcon}
+              quality={100}
+              width={62}
+              height={62}
+            />
+          )}
+          {title && <h2 className={utilStyles.defaultElementLineHeight}>{title}</h2>}
+        </div>
+      )}
+      {body?.map(
+        (item, index) =>
+          item.text && (
+            <div
+              className={clsx(
+                !item.isLastText
+                  ? clsx(
+                      utilStyles.defaultElementMarginBottom,
+                      utilStyles.embeddedParagraphLineHeight
+                    )
+                  : utilStyles.embeddedParagraphLineHeight,
+                item.isLinkExternal ? utilStyles.commonAnchor : undefined
+              )}
+              key={index}
+            >
+              {renderData(item.text, item.isLinkExternal ? insertLinkExternal : insertLinkInternal)}
+            </div>
+          )
+      )}
+      {children}
+    </div>
+  );
 };
 
 export default Textbox;
