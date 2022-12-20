0d9b78fc725b5369ce07cb9fa62b58e85beab2d4
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-20 16:11:22 -0500

    Built CloudResources

--- pages/services-cloud-resources/index.js
+++ pages/services-cloud-resources/index.js
@@ -21,6 +21,7 @@ const CloudResources = () => {
   const jumbotron = pageData.jumbotron;
   const lateralColumn1 = pageData.lateralColumn1;
   const lateralColumn2 = pageData.lateralColumn2;
+  const callToAction = pageData.callToAction;
   const tabSection = pageData.tabSection;
   const tabContent = tabSection?.tabLayout?.tabs[activeTabIndex]?.content;
 
@@ -76,6 +77,188 @@ const CloudResources = () => {
           }
           rightColumn={<RightColumn body={lateralColumn2?.rightColumn?.body} />}
           hasBackground={lateralColumn2?.hasBackground}
+        />
+        <TabSection>
+          {tabContent?.textbox?.titleImageSrcPath && tabContent?.textbox?.titleImageAlt && (
+            <Textbox
+              title={tabContent.textbox?.title}
+              titleImageSrcPath={tabContent.textbox.titleImageSrcPath}
+              titleImageAlt={tabContent.textbox.titleImageAlt}
+              body={tabContent.textbox?.body}
+              isInLateralColumn={tabContent.textbox?.isInLateralColumn}
+              hasBorderBottom={tabContent.textbox?.hasBorderBottom}
+            />
+          )}
+          <LateralColumn
+            title={tabContent?.lateralColumn1?.title}
+            isSmallTitle={tabContent?.lateralColumn1?.isSmallTitle}
+            titleImageSrcPath={tabContent?.lateralColumn1?.titleImageSrcPath}
+            titleImageAlt={tabContent?.lateralColumn1?.titleImageAlt}
+            leftColumn={<LeftColumn body={tabContent?.lateralColumn1?.leftColumn?.body} />}
+            rightColumn={
+              <RightColumn>
+                {tabContent?.lateralColumn1?.rightColumn?.image?.srcPath &&
+                tabContent?.lateralColumn1?.rightColumn?.image?.alt ? (
+                  <div className={utilStyles.childContainerAlignment}>
+                    <Image
+                      src={tabContent.lateralColumn1.rightColumn.image.srcPath}
+                      alt={tabContent.lateralColumn1.rightColumn.image.alt}
+                      className={utilStyles.tabSectionImage}
+                      priority={true}
+                      quality={100}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                ) : (
+                  <div className={utilStyles.maxWidth}>
+                    <Textbox
+                      title={tabContent?.lateralColumn1?.rightColumn?.textbox1?.title}
+                      body={tabContent?.lateralColumn1?.rightColumn?.textbox1?.body}
+                      isInLateralColumn={
+                        tabContent?.lateralColumn1?.rightColumn?.textbox1?.isInLateralColumn
+                      }
+                    />
+                    <div
+                      className={clsx(
+                        utilStyles.altChildContainer,
+                        utilStyles.childContainerAlignment
+                      )}
+                    >
+                      <BasicTable
+                        headRow={tabContent?.lateralColumn1?.rightColumn?.basicTable1?.headRow}
+                        body={tabContent?.lateralColumn1?.rightColumn?.basicTable1?.body}
+                      />
+                    </div>
+                    <div className={utilStyles.childContainer}>
+                      <Textbox
+                        title={tabContent?.lateralColumn1?.rightColumn?.textbox2?.title}
+                        body={tabContent?.lateralColumn1?.rightColumn?.textbox2?.body}
+                        isInLateralColumn={
+                          tabContent?.lateralColumn1?.rightColumn?.textbox2?.isInLateralColumn
+                        }
+                      />
+                    </div>
+                    <div
+                      className={clsx(
+                        utilStyles.altChildContainer,
+                        utilStyles.childContainerAlignment
+                      )}
+                    >
+                      <BasicTable
+                        headRow={tabContent?.lateralColumn1?.rightColumn?.basicTable2?.headRow}
+                        body={tabContent?.lateralColumn1?.rightColumn?.basicTable2?.body}
+                      />
+                    </div>
+                    <div className={utilStyles.childContainer}>
+                      <Textbox
+                        title={tabContent?.lateralColumn1?.rightColumn?.textbox3?.title}
+                        body={tabContent?.lateralColumn1?.rightColumn?.textbox3?.body}
+                        isInLateralColumn={
+                          tabContent?.lateralColumn1?.rightColumn?.textbox3?.isInLateralColumn
+                        }
+                      />
+                    </div>
+                    <div
+                      className={clsx(
+                        utilStyles.altChildContainer,
+                        utilStyles.childContainerAlignment
+                      )}
+                    >
+                      <BasicTable
+                        headRow={tabContent?.lateralColumn1?.rightColumn?.basicTable3?.headRow}
+                        body={tabContent?.lateralColumn1?.rightColumn?.basicTable3?.body}
+                      />
+                    </div>
+                    <div className={utilStyles.childContainer}>
+                      <Textbox
+                        title={tabContent?.lateralColumn1?.rightColumn?.textbox4?.title}
+                        body={tabContent?.lateralColumn1?.rightColumn?.textbox4?.body}
+                        isInLateralColumn={
+                          tabContent?.lateralColumn1?.rightColumn?.textbox4?.isInLateralColumn
+                        }
+                      />
+                    </div>
+                  </div>
+                )}
+              </RightColumn>
+            }
+            isAChild={true}
+            hasBorderBottom={tabContent?.lateralColumn1?.hasBorderBottom}
+          />
+          {tabContent?.textbox &&
+            (!tabContent?.textbox?.titleImageSrcPath || !tabContent?.textbox?.titleImageAlt) && (
+              <Textbox
+                title={tabContent.textbox?.title}
+                body={tabContent.textbox?.body}
+                isInLateralColumn={tabContent.textbox?.isInLateralColumn}
+                hasBorderBottom={tabContent.textbox?.hasBorderBottom}
+              />
+            )}
+          <LateralColumn
+            title={tabContent?.lateralColumn2?.title}
+            isSmallTitle={tabContent?.lateralColumn2?.isSmallTitle}
+            leftColumn={
+              <LeftColumn body={tabContent?.lateralColumn2?.leftColumn?.body}>
+                {tabContent?.lateralColumn2?.leftColumn?.listLine && (
+                  <div className={utilStyles.altChildContainer}>
+                    <ListLine
+                      isVertical={tabContent.lateralColumn2.leftColumn.listLine.isVertical}
+                      items={tabContent.lateralColumn2.leftColumn.listLine.items}
+                    />
+                  </div>
+                )}
+              </LeftColumn>
+            }
+            rightColumn={
+              <RightColumn>
+                <div className={utilStyles.childContainerAlignment}>
+                  <Image
+                    src={tabContent?.lateralColumn2?.rightColumn?.image?.srcPath}
+                    alt={tabContent?.lateralColumn2?.rightColumn?.image?.alt}
+                    className={utilStyles.tabSectionImage}
+                    priority={true}
+                    quality={100}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </RightColumn>
+            }
+            isAChild={true}
+            hasBorderBottom={tabContent?.lateralColumn2?.hasBorderBottom}
+          />
+          {tabContent?.lateralColumn3 && (
+            <LateralColumn
+              title={tabContent.lateralColumn3.title}
+              isSmallTitle={tabContent.lateralColumn3.isSmallTitle}
+              leftColumn={<LeftColumn body={tabContent?.lateralColumn3.leftColumn?.body} />}
+              rightColumn={
+                <RightColumn>
+                  <div className={utilStyles.childContainerAlignment}>
+                    <Image
+                      src={tabContent.lateralColumn3.rightColumn?.image?.srcPath}
+                      alt={tabContent.lateralColumn3.rightColumn?.image?.alt}
+                      className={utilStyles.tabSectionImage}
+                      priority={true}
+                      quality={100}
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                </RightColumn>
+              }
+              isAChild={true}
+              hasBorderBottom={tabContent.lateralColumn3.hasBorderBottom}
+            />
+          )}
+        </TabSection>
+        <CallToAction
+          title={callToAction?.title}
+          imageSrcPath={callToAction?.imageSrcPath}
+          imageAlt={callToAction?.imageAlt}
+          description={callToAction?.description}
+          buttonData={callToAction?.buttonData}
         />
       </div>
     </>
