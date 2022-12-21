20ca5aa188bf2a9e17d08eb27bbc885e9bf73f23
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-21 16:15:36 -0500

    Removed SearchBar

--- components/searchBar/searchBar.js
+++
@@ -1,9 +0,0 @@
-const SearchBar = ({ styleContainer, styleInput }) => {
-  return (
-    <div className={styleContainer}>
-      <input className={styleInput} type="text" name="search" placeholder="Search"></input>
-    </div>
-  );
-};
-
-export default SearchBar;
