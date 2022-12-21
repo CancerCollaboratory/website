6b6fe5a43b9043713be848ee2f7d4af2d2d82d9d
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-21 10:44:40 -0500

    Refactored FeatureBoard

--- components/featureBoard/featureBoard.js
+++ components/featureBoard/featureBoard.js
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import CustomLink from "../customLink/customLink";
+import Board from "../board/board";
 import utilStyles from "../../styles/utils.module.scss";
 import styles from "./featureBoard.module.scss";
 
@@ -11,24 +11,17 @@ const FeatureBoard = ({ boards }) => {
           return null;
         } else {
           return (
-            <div
-              className={clsx(
-                styles.innerContainer,
-                board.hasBackground ? styles.background : undefined,
-                index % 2 !== 0 ? styles.addBackground : undefined
-              )}
-              key={index}
-            >
-              {!board.titleLink && <p className={styles.title}>{board.title}</p>}
-              {board.titleLink && (
-                <CustomLink
-                  label={board.title}
-                  link={board.titleLink}
-                  isLinkExternal={board.isTitleLinkExternal}
-                  styleLabel={clsx(styles.titleLink, utilStyles.commonAnchor)}
-                />
-              )}
-              <p className={styles.caption}>{board.caption}</p>
+            <div className={clsx(styles.innerContainer, utilStyles.outerBoardMargins)} key={index}>
+              <Board
+                title={board.title}
+                titleLink={board.titleLink}
+                isTitleLinkExternal={board.isTitleLinkExternal}
+                caption={board.caption}
+                styleBackground={clsx(
+                  board.hasBackground ? utilStyles.boardBackground : undefined,
+                  index % 2 !== 0 ? utilStyles.swapBoardBackground : undefined
+                )}
+              />
             </div>
           );
         }
