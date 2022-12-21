b17e86de62c34bf36f0d21d8a84a6a5d136ea014
Dhyey Patel <dn6patel@uwaterloo.ca>
2022-12-21 17:00:41 -0500

    Built SoftwareForResearch

--- pages/research/software/index.js
+++ pages/research/software/index.js
@@ -1,5 +1,20 @@
+import Jumbotron from "../../../components/jumbotron/jumbotron";
+import SoftwareBoxLayout from "../../../components/softwareBoxLayout/softwareBoxLayout";
+import pageData from "../../../data/pages/research/software/software.json";
+import utilStyles from "../../../styles/utils.module.scss";
+
 const SoftwareForResearch = () => {
-  return;
+  const jumbotron = pageData.jumbotron;
+  const softwareBoxLayout = pageData.softwareBoxLayout;
+
+  return (
+    <>
+      <Jumbotron title={jumbotron?.title} bodyText={jumbotron?.body?.text} />
+      <div className={utilStyles.pageBodyContainer}>
+        <SoftwareBoxLayout SoftwareBoxes={softwareBoxLayout?.softwareBoxes} />
+      </div>
+    </>
+  );
 };
 
 export default SoftwareForResearch;
