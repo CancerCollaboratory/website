5e7c9e61c081df3d048c3d786c989521825491de
Dhyey Patel <97138744+dhyeypatell@users.noreply.github.com>
2022-12-20 21:22:18 -0500

    #115 - Build New Table Component
    * Built BasicTable
    
    * Styled BasicTable and made it responsive
    
    * Added and renamed color variables
    
    * Updated old color variables
    
    * Added and used util styling

---
+++ components/basicTable/basicTable.js
@@ -0,0 +1,41 @@
+import utilStyles from "../../styles/utils.module.scss";
+import styles from "./basicTable.module.scss";
+
+const BasicTable = ({ headRow, body }) => {
+  if (!headRow || !body) {
+    return null;
+  } else {
+    return (
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            {headRow.map(
+              (cell, index) =>
+                cell.value && (
+                  <th className={utilStyles.textOverflow} key={index}>
+                    {cell.value}
+                  </th>
+                )
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {body.map((row, index) => (
+            <tr className={index % 2 !== 0 ? styles.rowBackground : undefined} key={index}>
+              {row.map(
+                (cell, subIndex) =>
+                  cell.value && (
+                    <td className={utilStyles.textOverflow} key={subIndex}>
+                      {cell.value}
+                    </td>
+                  )
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+};
+
+export default BasicTable;
